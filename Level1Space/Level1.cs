0a9afd993fffc6834cf9c3204c4bbbc8b19598ec
Konstantin.Varlamov <Konstantin.Varlamov@norbit.ru>
2022-08-28 08:46:01 +0300

    task 1

---
+++ Level1Space/Level1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Level1Space
+{
+    public static class Level1
+    {
+        public static int squirrel(int N)
+        {
+            long result = 1;
+
+            for (int i = 2; i <= N; i++)
+            {
+                result = result * i;
+            }
+
+            while (result >= 10)
+            {
+                result = result / 10;
+            }
+            
+            return (int)result ;
+        }
+    }
+}
