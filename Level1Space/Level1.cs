b5dd6d36e08b62e62a95d44d473984c1cf98e3c0
Konstantin.Varlamov <Konstantin.Varlamov@norbit.ru>
2022-08-31 22:52:02 +0300

    refactoring

--- Level1Space/Level1.cs
+++ Level1Space/Level1.cs
@@ -7,37 +7,36 @@ namespace Level1Space
     {
         public static string PatternUnlock(int N, int [] hits)
         {
-
-            Dictionary<int, int[]> diagonals = new Dictionary<int, int[]>
-            {
-                {6, new int[] {2}},
-                {1, new int[] {5,8}},
-                {9, new int[] {2}},
-                {5, new int[] {1,3}},
-                {2, new int[] {6,9,4,7}},
-                {8, new int[] {1,3}},
-                {4, new int[] {2}},
-                {3, new int[] {5,8}},
-                {7, new int[] {2}}
-            };
+            double sqrTwo = Math.Sqrt(2);
+            Dictionary<int, int[]> diagonals = new Dictionary<int, int[]>();
+            diagonals.Add(6, new int[] {2});
+            diagonals.Add(1, new int[] {5,8});
+            diagonals.Add(9, new int[] {2});
+            diagonals.Add(5, new int[] {1,3});
+            diagonals.Add(2, new int[] {6,9,4,7});
+            diagonals.Add(8, new int[] {1,3});
+            diagonals.Add(4, new int[] {2});
+            diagonals.Add(3, new int[] {5,8});
+            diagonals.Add(7, new int[] {2});
 
             double sum = 0;
 
-            for(int i=0; i < N - 1; i++)
+            for(int i = 0; i < N - 1; i++)
             {
-                //long diagonal
-                if (((hits[i] == 6 || hits[i] == 7) && (hits[i + 1] == 6 || hits[i + 1] == 7)) || 
-                ((hits[i] == 9 || hits[i] == 4) && (hits[i + 1] == 9 || hits[i + 1] == 4)))
-                    sum += Math.Sqrt(2) * 2;
-                else if (Array.IndexOf(diagonals[hits[i]], hits[i+1]) != -1)
-                    sum += Math.Sqrt(2);
+                if (Array.IndexOf(diagonals[hits[i]], hits[i + 1]) != -1)
+                {
+                    sum += sqrTwo;
+                }
                 else
+                {
                     sum += 1;
+                }
             }
             
             sum = Math.Round(sum, 5);
+            string sumS = sum.ToString().Replace(".","").Replace("0","");
 
-            return sum.ToString().Replace(".","").Replace("0","");
+            return sumS;
         }
     }
 }
