325bd992d3daf9599e751a2fafa190bc454e3e8c
Konstantin.Varlamov <Konstantin.Varlamov@norbit.ru>
2022-08-31 18:42:11 +0300

    task 6

--- Level1Space/Level1.cs
+++ Level1Space/Level1.cs
@@ -5,25 +5,39 @@ namespace Level1Space
 {
     public static class Level1
     {
-        public static int [] SynchronizingTables(int N, int [] ids, int [] salary)
+        public static string PatternUnlock(int N, int [] hits)
         {
-            Dictionary<int, int> dictionary = new Dictionary<int, int>(N);
-            int[] idsSort = new int[N];
-            Array.Copy(ids, idsSort, N);
-            Array.Sort(idsSort);
-            Array.Sort(salary);
 
-            for (var i = 0; i < N; i++)
+            Dictionary<int, int[]> diagonals = new Dictionary<int, int[]>
             {
-                dictionary.Add(idsSort[i], salary[i]);
-            }
+                {6, new int[] {2}},
+                {1, new int[] {5,8}},
+                {9, new int[] {2}},
+                {5, new int[] {1,3}},
+                {2, new int[] {6,9,4,7}},
+                {8, new int[] {1,3}},
+                {4, new int[] {2}},
+                {3, new int[] {5,8}},
+                {7, new int[] {2}}
+            };
 
-            for (int i = 0; i < N; i++)
+            double sum = 0;
+
+            for(int i=0; i < N - 1; i++)
             {
-                salary[i] = dictionary[ids[i]];
+                //long diagonal
+                if (((hits[i] == 6 || hits[i] == 7) && (hits[i + 1] == 6 || hits[i + 1] == 7)) || 
+                ((hits[i] == 9 || hits[i] == 4) && (hits[i + 1] == 9 || hits[i + 1] == 4)))
+                    sum += Math.Sqrt(2) * 2;
+                else if (Array.IndexOf(diagonals[hits[i]], hits[i+1]) != -1)
+                    sum += Math.Sqrt(2);
+                else
+                    sum += 1;
             }
+            
+            sum = Math.Round(sum, 5);
 
-            return salary;
+            return sum.ToString().Replace(".","").Replace("0","");
         }
     }
 }
--- Level1Space/Level1Solved.cs
+++ Level1Space/Level1Solved.cs
@@ -126,5 +126,26 @@ namespace Level1Space
     
             return result;
         }
+        
+        public static int [] SynchronizingTables(int N, int [] ids, int [] salary)
+        {
+            Dictionary<int, int> dictionary = new Dictionary<int, int>(N);
+            int[] idsSort = new int[N];
+            Array.Copy(ids, idsSort, N);
+            Array.Sort(idsSort);
+            Array.Sort(salary);
+
+            for (var i = 0; i < N; i++)
+            {
+                dictionary.Add(idsSort[i], salary[i]);
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                salary[i] = dictionary[ids[i]];
+            }
+
+            return salary;
+        }
     }
 }
