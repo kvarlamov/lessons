5fae7672fe00554f181b87d8edf5a3a7695ee5f8
Konstantin.Varlamov <Konstantin.Varlamov@norbit.ru>
2022-09-01 11:23:53 +0300

    Remove math, added checks

--- Level1Space/Level1.cs
+++ Level1Space/Level1.cs
@@ -7,8 +7,14 @@ namespace Level1Space
     {
         public static string PatternUnlock(int N, int [] hits)
         {
-            double sqrTwo = Math.Sqrt(2);
-            Dictionary<int, int[]> diagonals = new Dictionary<int, int[]>();
+            if (N == 0)
+                return string.Empty;
+            
+            if (N == 1)
+                return hits[0].ToString();
+            
+            double sqrTwo = 1.414213562373;
+            var diagonals = new Dictionary<int, int[]>();
             diagonals.Add(1, new int [] {5,8});
             diagonals.Add(2, new int [] {6,9,4,7});
             diagonals.Add(3, new int [] {5,8});
@@ -23,7 +29,8 @@ namespace Level1Space
 
             for(int i = 0; i < N - 1; i++)
             {
-                if (Array.IndexOf(diagonals[hits[i]], hits[i + 1]) != -1)
+                bool flag = diagonals.TryGetValue(hits[i], out var array);
+                if (flag && Array.IndexOf(array, hits[i + 1]) != -1)
                 {
                     sum += sqrTwo;
                 }
@@ -32,11 +39,26 @@ namespace Level1Space
                     sum += 1;
                 }
             }
-            
-            sum = Math.Round(sum, 5);
-            string sumS = sum.ToString().Replace(".","").Replace("0","");
 
-            return sumS;
+            string sums = sum.ToString();
+            string[] split = sums.Split('.');
+            if (split.Length > 1)
+            {
+                double roundChar = Char.GetNumericValue(split[1][5]);
+                if (roundChar > 4)
+                {
+                    double toChange = Char.GetNumericValue(split[1][4]) + 1;
+                    split[1] = split[1].Remove(4, 1);
+                    split[1] = split[1].Insert(4, toChange.ToString());
+                }
+
+                split[1] = split[1].Substring(0, 5);
+            }
+
+            string result = string.Join("", split);
+            result = result.Replace(".", "").Replace("0", "");
+
+            return result;
         }
     }
 }
