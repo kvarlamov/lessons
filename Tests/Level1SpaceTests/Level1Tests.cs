c9b159b5728de9c072468033d68f036b744033ac
Konstantin.Varlamov <Konstantin.Varlamov@norbit.ru>
2022-09-01 12:23:57 +0300

    added tests

---
+++ Tests/Level1SpaceTests/Level1Tests.cs
@@ -0,0 +1,64 @@
+﻿using Level1Space;
+using NUnit.Framework;
+
+namespace Level1SpaceTests
+{
+    public class Level1Tests
+    {
+        [Test]
+        public void PatternUnlock_receive_141421()
+        {
+            // Arrange
+            var hits = new int[] {4, 2};
+            int n = 2;
+            
+            // Act
+            var result = Level1.PatternUnlock(n, hits);
+            
+            // Assert
+            Assert.AreEqual("141421", result);
+        }
+        
+        [Test]
+        public void PatternUnlock_receive_982843()
+        {
+            // Arrange
+            var hits = new int[] {1,2,3,4,5,6,2,7,8,9};
+            int n = 10;
+            
+            // Act
+            var result = Level1.PatternUnlock(n, hits);
+            
+            // Assert
+            Assert.AreEqual("982843", result);
+        }
+        
+        [Test]
+        public void PatternUnlock_receive_2()
+        {
+            // Arrange
+            var hits = new int[] {9, 8, 7};
+            int n = 3;
+            
+            // Act
+            var result = Level1.PatternUnlock(n, hits);
+            
+            // Assert
+            Assert.AreEqual("2", result);
+        }
+        
+        [Test]
+        public void PatternUnlock_receive_1()
+        {
+            // Arrange
+            var hits = new int[] {1,9,8,7,3,2,5,6,1,2,8};
+            int n = 11;
+            
+            // Act
+            var result = Level1.PatternUnlock(n, hits);
+            
+            // Assert
+            Assert.AreEqual("1", result);
+        }
+    }
+}
---
+++ Tests/Level1SpaceTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Level1SpaceTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Level1SpaceTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6b464dc4-092d-446d-940d-8e247d219d8f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
