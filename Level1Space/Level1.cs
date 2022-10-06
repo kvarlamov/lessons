623db28e86c4783f2c3850a9fe657c6a58af3434
Konstantin.Varlamov <Konstantin.Varlamov@norbit.ru>
2022-10-06 08:15:17 +0300

    fix

--- Level1Space/Level1.cs
+++ Level1Space/Level1.cs
@@ -7,7 +7,7 @@ namespace Level1Space
     {
         public static bool TransformTransform(int[] A, int N)
         {
-            int[] transform = Transform(A);
+            List<int> transform = Transform(A);
 
             var result = 0;
 
@@ -19,28 +19,20 @@ namespace Level1Space
             return result % 2 == 0;
         }
 
-        private static int[] Transform(int[] A)
+        private static List<int> Transform(int[] A)
         {
-            int numberOfTransformation = 2;
-            List<int> B = new List<int>();
-
-            for (int n = 0; n < numberOfTransformation; n++)
-            {
-                B = Transformate(A);
-                A = B.ToArray();
-            }
-
-            return B.ToArray();
+            return Transformate(Transformate(A).ToArray());
         }
 
         private static List<int> Transformate(int[] A)
         {
             int n = A.Length;
+
             List<int> B = new List<int>();
-            
-            for (int i = 0; i < n - 1; i++)
+
+            for (int i = 0; i <= n - 1; i++)
             {
-                for (int j = 0; j < n - 1 - i; j++)
+                for (int j = 0; j <= n - 1 - i; j++)
                 {
                     int k = i + j;
                     int max = FindMax(A, j, k);
@@ -55,7 +47,7 @@ namespace Level1Space
         {
             int max = 0;
             
-            for (int i = j; i < k; i++)
+            for (int i = j; i <= k; i++)
             {
                 if (A[i] > max)
                     max = A[i];
