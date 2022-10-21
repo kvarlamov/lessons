b2e363627431cd0f30f461fbe6842593a8481430
Konstantin.Varlamov <Konstantin.Varlamov@norbit.ru>
2022-10-21 08:55:51 +0300

    Dynamic array

---
+++ AlgorithmsDataStructures/DynArray.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+    public class DynArray<T>
+    {
+        private const int MinimumCapacity = 16;
+        public T [] array;
+        public int count;
+        public int capacity;
+
+        public DynArray()
+        {
+            count = 0;
+            MakeArray(16);
+        }
+
+        public void MakeArray(int new_capacity)
+        {
+            Array.Resize(ref array, new_capacity);
+            capacity = new_capacity;
+        }
+
+        /// <summary>
+        /// Get by index
+        /// </summary>
+        /// <param name="index">index of elemet</param>
+        /// <returns>item</returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public T GetItem(int index)
+        {
+            CheckIndex(index);
+            
+            return array[index];
+        }
+
+        /// <summary>
+        /// Append to tail
+        /// </summary>
+        /// <param name="itm">new item</param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public void Append(T itm)
+        {
+            ResizeIfFull();
+
+            array[count] = itm;
+            count++;
+        }
+
+        /// <summary>
+        /// insert in index
+        /// </summary>
+        /// <param name="itm">new item</param>
+        /// <param name="index">index to insert</param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public void Insert(T itm, int index)
+        {
+            CheckIndex(index);
+            ResizeIfFull();
+            
+            for (int i = index; i <= count; i++)
+            {
+                T tmp = array[i];
+                array[i] = itm;
+                itm = tmp;
+            }
+
+            count++;
+        }
+
+        /// <summary>
+        /// Remove
+        /// </summary>
+        /// <param name="index"></param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public void Remove(int index)
+        {
+            CheckIndex(index);
+
+            for (int i = index; i < count - 1; i++)
+            {
+                array[i] = array[i + 1];
+            }
+
+            count--;
+            ShrinkArr();
+        }
+
+        /// <summary>
+        /// resize array if it's full
+        /// </summary>
+        private void ResizeIfFull()
+        {
+            if (count == capacity)
+            {
+                MakeArray(capacity * 2);
+            }
+        }
+
+        /// <summary>
+        /// shrink if count < 50% of capacity
+        /// </summary>
+        private void ShrinkArr()
+        {
+            double result = (double)count / capacity;
+            if (result >= 0.5)
+                return;
+
+            int newCapacity = (int)(capacity / 1.5);
+            if (newCapacity <= MinimumCapacity)
+                MakeArray(MinimumCapacity);
+            else
+                MakeArray(newCapacity);
+        }
+
+        private void CheckIndex(int index)
+        {
+            if (index > count || index < 0)
+                throw new IndexOutOfRangeException();
+        }
+    }
+}
---
+++ Tests/AlgorithmsDataStructuresTests/DynArrayTests.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using AlgorithmsDataStructures;
+using NUnit.Framework;
+
+namespace AlgorithmsDataStructuresTests
+{
+    public class DynArrayTests
+    {
+        [Test]
+        public void Insert_1()
+        {
+            DynArray<int> array = new DynArray<int>();
+            FillArray(array);
+            
+            //act
+            array.Insert(2, 1);
+            
+            //assert
+            Assert.AreEqual(2, array.GetItem(1));
+            Assert.AreEqual(1, array.GetItem(0));
+            Assert.AreEqual(7, array.GetItem(6));
+            Assert.AreEqual(7, array.count);
+            Assert.AreEqual(16, array.capacity);
+        }
+        
+        [Test]
+        public void Insert_0()
+        {
+            DynArray<int> array = new DynArray<int>();
+            FillArrayFullCapacity(array);
+            
+            //act
+            array.Insert(2, 0);
+            
+            //assert
+            Assert.AreEqual(1, array.GetItem(1));
+            Assert.AreEqual(2, array.GetItem(0));
+            Assert.AreEqual(1, array.GetItem(16));
+            Assert.AreEqual(17, array.count);
+            Assert.AreEqual(32, array.capacity);
+        }
+        
+        [Test]
+        public void Insert_Tail()
+        {
+            DynArray<int> array = new DynArray<int>();
+            FillArray(array);
+            
+            //act
+            array.Insert(8, 6);
+            
+            //assert
+            Assert.AreEqual(1, array.GetItem(0));
+            Assert.AreEqual(7, array.GetItem(5));
+            Assert.AreEqual(8, array.GetItem(6));
+            Assert.AreEqual(7, array.count);
+            Assert.AreEqual(16, array.capacity);
+        }
+
+        [Test]
+        [TestCase(-1)]
+        [TestCase(16)]
+        public void WrongInsert(int i)
+        {
+            DynArray<int> array = new DynArray<int>();
+            FillArray(array);
+
+            Assert.Throws<IndexOutOfRangeException>(() => array.Insert(1, i));
+        }
+
+        [Test]
+        public void RemoveWithoutChange_Head()
+        {
+            DynArray<int> array = new DynArray<int>();
+            FillArrayFullCapacity2(array);
+            
+            Assert.AreEqual(64, array.capacity);
+            
+            array.Remove(0);
+            
+            Assert.AreEqual(1, array.GetItem(0));
+            Assert.AreEqual(64, array.capacity);
+            Assert.AreEqual(32, array.GetItem(32));
+            Assert.AreEqual(32, array.count);
+        }
+        
+        [Test]
+        public void RemoveWithShrink_Head()
+        {
+            DynArray<int> array = new DynArray<int>();
+            FillArrayFullCapacity2(array);
+            
+            Assert.AreEqual(64, array.capacity);
+            
+            array.Remove(0);
+            array.Remove(0);
+            
+            Assert.AreEqual(2, array.GetItem(0));
+            Assert.AreEqual(42, array.capacity);
+            Assert.AreEqual(32, array.GetItem(31));
+            Assert.AreEqual(31, array.count);
+        }
+        
+        [Test]
+        public void RemoveWithShrinkAndException_Head()
+        {
+            DynArray<int> array = new DynArray<int>();
+            FillArrayFullCapacity2(array);
+            
+            Assert.AreEqual(64, array.capacity);
+            
+            array.Remove(0);
+            array.Remove(0);
+            Assert.Throws<IndexOutOfRangeException>(() => array.Remove(32));
+        }
+
+        private void FillArray(DynArray<int> arr)
+        {
+            arr.Append(1);
+            arr.Append(3);
+            arr.Append(4);
+            arr.Append(5);
+            arr.Append(6);
+            arr.Append(7);
+        }
+        
+        private void FillArrayFullCapacity(DynArray<int> arr)
+        {
+            //16
+            arr.Append(1);
+            arr.Append(1);
+            arr.Append(1);
+            arr.Append(1);
+            arr.Append(1);
+            arr.Append(1);
+            arr.Append(1);
+            arr.Append(1);
+            arr.Append(1);
+            arr.Append(1);
+            arr.Append(1);
+            arr.Append(1);
+            arr.Append(1);
+            arr.Append(1);
+            arr.Append(1);
+            arr.Append(1);
+        }
+        
+        private void FillArrayFullCapacity2(DynArray<int> arr)
+        {
+            //33
+            for (int i = 0; i < 33; i++)
+            {
+                arr.Append(i);
+            }
+        }
+    }
+}
