785c3abeec8994eb78c95a11fa59891ee5cd7535
Konstantin.Varlamov <Konstantin.Varlamov@norbit.ru>
2022-10-25 21:32:09 +0300

    Deque

---
+++ AlgorithmsDataStructures/Deque.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+    public class Deque<T>
+    {
+        private List<T> _deque;
+        public Deque()
+        {
+            _deque = new List<T>();
+        }
+
+        public void AddFront(T item)
+        {
+            _deque.Add(item);
+        }
+
+        public void AddTail(T item)
+        {
+            _deque.Insert(0, item);
+        }
+
+        public T RemoveFront()
+        {
+            if (Size() == 0)
+                return default(T);
+
+            T item = _deque[_deque.Count - 1];
+            _deque.RemoveAt(_deque.Count - 1);
+            return item;
+        }
+
+        public T RemoveTail()
+        {
+            if (Size() == 0)
+                return default(T);
+
+            T item = _deque[0];
+            _deque.RemoveAt(0);
+            return item;
+        }
+        
+        public int Size()
+        {
+            return _deque.Count;
+        }
+    }
+
+    public static class DequeTasks
+    {
+        public static bool IsPalindrome(string s)
+        {
+            Deque<char> deque = new Deque<char>();
+            string pattern = "[^A-Za-zЁёА-я]";
+            s = System.Text.RegularExpressions.Regex.Replace(s, pattern, string.Empty);
+            for (int i = 0; i < s.Length; i++)
+            {
+                deque.AddFront(s[i]);
+            }
+            int stop = 0;
+            if (s.Length % 2 != 0)
+                stop = 1;
+            
+            while (deque.Size() > stop)
+            {
+                //a
+                //121
+                //1221
+                var first = deque.RemoveFront().ToString();
+                var second = deque.RemoveTail().ToString();
+                if (!string.Equals(first, second, StringComparison.OrdinalIgnoreCase))
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Tests/AlgorithmsDataStructuresTests/DequeTests.cs
@@ -0,0 +1,96 @@
+﻿using AlgorithmsDataStructures;
+using NUnit.Framework;
+
+namespace AlgorithmsDataStructuresTests
+{
+    public class DequeTests
+    {
+        [Test]
+        public void DequeLikeStack()
+        {
+            Deque<int> deque = new Deque<int>();
+            Assert.That(deque.Size(), Is.EqualTo(0));
+            
+            deque.AddFront(1);
+            Assert.That(deque.Size(), Is.EqualTo(1));
+            deque.AddFront(2);
+            Assert.That(deque.Size(), Is.EqualTo(2));
+            Assert.That(deque.RemoveFront(), Is.EqualTo(2));
+            deque.AddFront(1);
+            deque.AddTail(3);
+            deque.AddTail(4);
+            Assert.That(deque.RemoveTail(), Is.EqualTo(4));
+            Assert.That(deque.Size(), Is.EqualTo(3));
+            Assert.That(deque.RemoveFront(), Is.EqualTo(1));
+            Assert.That(deque.Size(), Is.EqualTo(2));
+            Assert.That(deque.RemoveFront(), Is.EqualTo(1));
+            Assert.That(deque.RemoveFront(), Is.EqualTo(3));
+            Assert.That(deque.RemoveTail(), Is.EqualTo(0));
+            Assert.That(deque.RemoveFront(), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void DequeLikeStack2()
+        {
+            Deque<int> deque = new Deque<int>();
+            deque.AddFront(1);
+            deque.AddFront(2);
+            deque.AddFront(3);
+            Assert.That(deque.RemoveFront(), Is.EqualTo(3));
+            Assert.That(deque.RemoveFront(), Is.EqualTo(2));
+            Assert.That(deque.RemoveFront(), Is.EqualTo(1));
+            
+            deque.AddTail(1);
+            deque.AddTail(2);
+            deque.AddTail(3);
+            Assert.That(deque.RemoveTail(), Is.EqualTo(3));
+            Assert.That(deque.RemoveTail(), Is.EqualTo(2));
+            Assert.That(deque.RemoveTail(), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void DequeLikeQueue()
+        {
+            Deque<int> deque = new Deque<int>();
+            deque.AddFront(1);
+            deque.AddFront(2);
+            deque.AddFront(3);
+            Assert.That(deque.RemoveTail(), Is.EqualTo(1));
+            Assert.That(deque.RemoveTail(), Is.EqualTo(2));
+            Assert.That(deque.RemoveTail(), Is.EqualTo(3));
+            Assert.That(deque.Size(), Is.EqualTo(0));
+            
+            deque.AddTail(1);
+            deque.AddTail(2);
+            deque.AddTail(3);
+            Assert.That(deque.RemoveFront(), Is.EqualTo(1));
+            Assert.That(deque.RemoveFront(), Is.EqualTo(2));
+            Assert.That(deque.RemoveFront(), Is.EqualTo(3));
+        }
+
+        [Test]
+        [TestCase("121", true)]
+        [TestCase("1221", true)]
+        [TestCase("Alula", true)]
+        [TestCase("Civic", true)]
+        [TestCase("Madam", true)]
+        [TestCase("Noon", true)]
+        [TestCase("My gym", true)]
+        [TestCase("Don’t nod", true)]
+        [TestCase("Was it a cat I saw?", true)]
+        [TestCase("Step on no pets.", true)]
+        [TestCase("Do geese see God", true)]
+        [TestCase("abc", false)]
+        [TestCase("ab", false)]
+        [TestCase("aa", true)]
+        [TestCase("false", false)]
+        [TestCase("No, Mel Gibson is a casino’s big lemon", true)]
+        [TestCase("Cigar? Toss it in a can. It is so tragic", true)]
+        [TestCase("No, sir, prefer prison", true)]
+        [TestCase("Naomi, did I moan?", true)]
+        public void IsPalindrome(string s, bool expected)
+        {
+            Assert.That(DequeTasks.IsPalindrome(s), Is.EqualTo(expected)); 
+        }
+    }
+}
