4932ef3728732aa796dd360bb624a2ad91991b2f
Konstantin.Varlamov <Konstantin.Varlamov@norbit.ru>
2022-10-30 13:11:26 +0300

    Ordered list

---
+++ AlgorithmsDataStructures/OrderedList.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+    public class Node<T>
+    {
+        public T value;
+        public Node<T> next, prev;
+
+        public Node(T _value)
+        {
+            value = _value;
+            next = null;
+            prev = null;
+        }
+    }
+
+    public class OrderedList<T>
+    {
+        public Node<T> head, tail;
+        private bool _ascending;
+
+        public OrderedList(bool asc)
+        {
+            head = null;
+            tail = null;
+            _ascending = asc;
+        }
+
+        public int Compare(T v1, T v2)
+        {
+            int result = 0;
+            if(typeof(T) == typeof(String))
+            {
+                throw new NotImplementedException();
+                // версия для лексикографического сравнения строк
+            }
+            else
+            {
+                //int only implemented for now
+                result = CompareUniversal(v1, v2);
+            }
+      
+            return result;
+            // -1 if v1 < v2
+            // 0 if v1 == v2
+            // +1 if v1 > v2
+        }
+
+        public void Add(T value)
+        {
+            var newNode = new Node<T>(value);
+
+            if (head == null)
+            {
+                InsertFirst(newNode);
+                return;
+            }
+            
+            var current = head;
+
+            while (current.next != null)
+            {
+                // check that pair of values current and next are equal with insert
+                if (
+                    GetCompareResult(value, current.value) ||
+                    (Compare(current.value, current.next.value) == 0 && Compare(value, current.value) == 0)
+                    )
+                {
+                    current = current.next;
+                    continue;
+                }
+
+                InsertBefore(current, newNode);
+
+                return;
+            }
+            
+            InsertHeadOrTail(current, newNode);
+        }
+
+        public Node<T> Find(T val)
+        {
+            var list = GetAll();
+
+            return BinarySearch(list, 0, list.Count - 1, val);
+            // var current = head;
+            //
+            // while (current != null)
+            // {
+            //     if (current.value.Equals(val))
+            //         return current;
+            //
+            //     current = current.next;
+            // }
+        }
+
+        private Node<T> BinarySearch(List<Node<T>> list, int left, int right, T val)
+        {
+            if (left > right)
+            {
+                return null;
+            }
+
+            int middle = (left + right) / 2;
+            var compare = Compare(val, list[middle].value);
+            if (compare == 0)
+                return list[middle];
+
+            bool compareStrategy = _ascending ? compare > 0 : compare < 0;
+            if (compareStrategy)
+                left++;
+            else
+                right--;
+
+            return BinarySearch(list, left, right, val);
+        }
+
+        public void Delete(T val)
+        {
+            var current = head;
+
+            while (current != null)
+            {
+                if (!current.value.Equals(val))
+                {
+                    current = current.next;
+                    continue;
+                }
+
+                //head case
+                if (current.prev == null)
+                {
+                    RemoveHead(current);
+                    return;
+                }
+
+                //tail case
+                if (current.next == null)
+                {
+                    tail = current.prev;
+                    tail.next = null;
+                    return;
+                }
+
+                // move pointers
+                current.prev.next = current.next;
+                current.next.prev = current.prev;
+                return;
+            }
+        }
+
+        
+
+        public void Clear(bool asc)
+        {
+            _ascending = asc;
+            head = null;
+            tail = null;
+        }
+
+        public int Count()
+        {
+            int count = 0;
+            var current = head;
+            while (current != null)
+            {
+                count++;
+                current = current.next;
+            }
+
+            return count;
+        }
+
+        public List<Node<T>> GetAll() // выдать все элементы упорядоченного 
+            // списка в виде стандартного списка
+        {
+            var r = new List<Node<T>>();
+            var node = head;
+            
+            while(node != null)
+            {
+                r.Add(node);
+                node = node.next;
+            }
+            return r;
+        }
+        
+        private int CompareUniversal(T v1, T v2)
+        {
+            if (typeof(T) != typeof(int))
+            {
+                return 0;
+            }
+
+            int a = (int) (object) v1;
+            int b = (int) (object) v2;
+
+            return a.CompareTo(b);
+        }
+
+        private void InsertFirst(Node<T> newNode)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+
+        private void InsertHeadOrTail(Node<T> current, Node<T> newNode)
+        {
+            if (GetCompareResult(newNode.value,current.value))
+            {
+                current.next = newNode;
+                newNode.prev = current;
+                tail = newNode;
+                return;
+            }
+
+            var tmp = current.prev;
+            if (tmp == null)
+            {
+                InsertBeforeHead(newNode);
+                return;                
+            }
+            
+            newNode.next = current;
+            current.prev = newNode;
+            newNode.prev = tmp;
+            tmp.next = newNode;
+        }
+        
+        private void InsertBeforeHead(Node<T> newNode)
+        {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+        
+        private void InsertBefore(Node<T> current, Node<T> newNode)
+        {
+            if (current.prev == null)
+            {
+                InsertBeforeHead(newNode);
+                return;
+            }
+            
+            var tmp = current.prev;
+            current.prev = newNode;
+            newNode.prev = tmp;
+            tmp.next = newNode;
+            newNode.next = current;
+        }
+        
+        private bool GetCompareResult(T value, T current)
+        {
+            int res = Compare(value, current);
+
+            return _ascending ? res >= 0 : res < 0;
+        }
+        
+        private void RemoveHead(Node<T> current)
+        {
+            head = current.next;
+
+            //there was only one element and now empty
+            if (head == null)
+            {
+                tail = null;
+                return;
+            }
+
+            head.prev = null;
+
+            //head and point the same
+            if (head.next == null)
+            {
+                tail.prev = null;
+            }
+        }
+    }
+}
---
+++ Tests/AlgorithmsDataStructuresTests/OrderedListTests.cs
@@ -0,0 +1,527 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using AlgorithmsDataStructures;
+using NUnit.Framework;
+
+namespace AlgorithmsDataStructuresTests
+{
+    public class OrderedListTests
+    {
+        #region Add_Asc
+
+        [Test]
+        public void Add_Head_Empty()
+        {
+            var list = new OrderedList<int>(true);
+            
+            list.Add(1);
+            
+            Assert.That(list.Count(), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Add_Head()
+        {
+            var list = new OrderedList<int>(true);
+            list.Add(2);
+            list.Add(1);
+            list.Add(3);
+
+            var listFromOrdered = list.GetAll();
+            Assert.That(listFromOrdered[0].value, Is.EqualTo(1));
+            Assert.That(list.Count(), Is.EqualTo(3));
+            Assert.That(listFromOrdered[2].value, Is.EqualTo(3));
+            Assert.That(list.head.value, Is.EqualTo(1));
+            Assert.That(list.tail.value, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Add_Middle_Test()
+        {
+            var list = new OrderedList<int>(true);
+            list.Add(3);
+            list.Add(2);
+            list.Add(1);
+            list.Add(4);
+            list.Add(6);
+            list.Add(5);
+
+            var result = list.GetAll();
+            var values = result.Select(x => x.value).ToList();
+
+            var expected = new List<int>()
+            {
+                1, 2, 3, 4, 5, 6
+            };
+            
+            CollectionAssert.AreEqual(expected, values);
+            Assert.That(list.head.value, Is.EqualTo(1));
+            Assert.That(list.Count(), Is.EqualTo(6));
+            Assert.That(list.tail.value, Is.EqualTo(6));
+            Assert.That(result.Last().value, Is.EqualTo(6));
+            Assert.That(result.First().value, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Add_Tail()
+        {
+            var list = new OrderedList<int>(true);
+            list.Add(2);
+            list.Add(5);
+
+            var result = list.GetAll();
+            Assert.That(result[0].value, Is.EqualTo(2));
+            Assert.That(result[1].value, Is.EqualTo(5));
+            Assert.That(list.tail.value, Is.EqualTo(5));
+            Assert.That(list.head.value, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Add_Middle_Equal1()
+        {
+            // 1 1 2 3 4, insert 1
+            var list = new OrderedList<int>(true);
+            list.Add(2);
+            list.Add(3);
+            list.Add(1);
+            list.Add(1);
+            list.Add(1);
+
+            var result = list.GetAll();
+            var values = result.Select(x => x.value).ToList();
+
+            var expected = new List<int>()
+            {
+                1, 1, 1, 2, 3
+            };
+            
+            CollectionAssert.AreEqual(expected, values);
+            Assert.That(list.head.value, Is.EqualTo(1));
+            Assert.That(list.Count(), Is.EqualTo(5));
+            Assert.That(list.tail.value, Is.EqualTo(3));
+            Assert.That(result.Last().value, Is.EqualTo(3));
+            Assert.That(result.First().value, Is.EqualTo(1));
+        }
+
+        #endregion
+
+        #region Add_Desc
+
+        [Test]
+        public void AddDesc_Head_Empty()
+        {
+            var list = new OrderedList<int>(false);
+            
+            list.Add(1);
+            
+            Assert.That(list.Count(), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void AddDesc_Head()
+        {
+            var list = new OrderedList<int>(false);
+            list.Add(2);
+            list.Add(1);
+            list.Add(3);
+
+            var listFromOrdered = list.GetAll();
+            Assert.That(listFromOrdered[0].value, Is.EqualTo(3));
+            Assert.That(list.Count(), Is.EqualTo(3));
+            Assert.That(listFromOrdered[2].value, Is.EqualTo(1));
+            Assert.That(list.head.value, Is.EqualTo(3));
+            Assert.That(list.tail.value, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void AddDesc_Middle_Test()
+        {
+            var list = new OrderedList<int>(false);
+            list.Add(3);
+            list.Add(2);
+            list.Add(1);
+            list.Add(4);
+            list.Add(6);
+            list.Add(5);
+
+            var result = list.GetAll();
+            var values = result.Select(x => x.value).ToList();
+
+            var expected = new List<int>()
+            {
+                6,5,4,3,2,1
+            };
+            
+            CollectionAssert.AreEqual(expected, values);
+            Assert.That(list.head.value, Is.EqualTo(6));
+            Assert.That(list.Count(), Is.EqualTo(6));
+            Assert.That(list.tail.value, Is.EqualTo(1));
+            Assert.That(result.Last().value, Is.EqualTo(1));
+            Assert.That(result.First().value, Is.EqualTo(6));
+        }
+
+        [Test]
+        public void AddDesc_Tail()
+        {
+            var list = new OrderedList<int>(false);
+            list.Add(2);
+            list.Add(5);
+
+            var result = list.GetAll();
+            Assert.That(result[0].value, Is.EqualTo(5));
+            Assert.That(result[1].value, Is.EqualTo(2));
+            Assert.That(list.tail.value, Is.EqualTo(2));
+            Assert.That(list.head.value, Is.EqualTo(5));
+        }
+        
+        [Test]
+        public void Add_Desc_Middle_Equal1()
+        {
+            var list = new OrderedList<int>(false);
+            list.Add(2);
+            list.Add(3);
+            list.Add(1);
+            list.Add(1);
+            list.Add(1);
+
+            var result = list.GetAll();
+            var values = result.Select(x => x.value).ToList();
+
+            var expected = new List<int>()
+            {
+                3,2, 1, 1, 1
+            };
+            
+            CollectionAssert.AreEqual(expected, values);
+            Assert.That(list.head.value, Is.EqualTo(3));
+            Assert.That(list.Count(), Is.EqualTo(5));
+            Assert.That(list.tail.value, Is.EqualTo(1));
+            Assert.That(result.Last().value, Is.EqualTo(1));
+            Assert.That(result.First().value, Is.EqualTo(3));
+        }
+
+        #endregion
+        
+        #region Delete
+
+        [Test]
+        public void Delete_EmptyList()
+        {
+            var list = new OrderedList<int>(true);
+
+            list.Delete(2);
+            var result = list.GetAll();
+            
+            Assert.AreEqual(0, result.Count);
+            Assert.AreEqual(null, list.head);
+            Assert.AreEqual(null, list.tail);
+        }
+        
+        [Test]
+        public void Delete_OneElementList_Deleted()
+        {
+            var list = new OrderedList<int>(false);
+            list.Add(2);
+
+            list.Delete(2);
+            var result = list.GetAll();
+            
+            Assert.AreEqual(0, result.Count);
+            Assert.AreEqual(null, list.head);
+            Assert.AreEqual(null, list.tail);
+        }
+        
+        [Test]
+        public void Delete_TwoElementListRemoveFirst_Deleted()
+        {
+            var list = new OrderedList<int>(true);
+            list.Add(3);
+            list.Add(2);
+
+            list.Delete(2);
+            var result = list.GetAll();
+            
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(3, list.head.value);
+            Assert.AreEqual(3, list.tail.value);
+            Assert.AreEqual(null, list.head.next);
+            Assert.AreEqual(null, list.tail.next);
+            Assert.IsNull(list.head.prev);
+            Assert.IsNull(list.tail.prev);
+        }
+        
+        [Test]
+        public void DeleteDesc_TwoElementListRemoveFirst_Deleted()
+        {
+            var list = new OrderedList<int>(false);
+            list.Add(2);
+            list.Add(3);
+
+            list.Delete(2);
+            var result = list.GetAll();
+            
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(3, list.head.value);
+            Assert.AreEqual(3, list.tail.value);
+            Assert.AreEqual(null, list.head.next);
+            Assert.AreEqual(null, list.tail.next);
+            Assert.IsNull(list.head.prev);
+            Assert.IsNull(list.tail.prev);
+        }
+
+        [Test]
+        public void Delete_ValueInHead_Deleted()
+        {
+            var list = new OrderedList<int>(true);
+            list.Add(2);
+            list.Add(3);
+            list.Add(2);
+
+            list.Delete(2);
+            var result = list.GetAll();
+            
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual(2, list.head.value);
+            Assert.AreEqual(3, list.tail.value);
+            Assert.IsNull(list.head.prev);
+        }
+        
+        [Test]
+        public void DeleteDesc_ValueInHead_Deleted()
+        {
+            var list = new OrderedList<int>(false);
+            list.Add(2);
+            list.Add(3);
+            list.Add(2);
+
+            list.Delete(2);
+            var result = list.GetAll();
+            
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual(3, list.head.value);
+            Assert.AreEqual(2, list.tail.value);
+            Assert.IsNull(list.head.prev);
+        }
+        
+        [Test]
+        public void Delete_ValueInTail_Deleted()
+        {
+            var list = new OrderedList<int>(true);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+
+            list.Delete(4);
+            var result = list.GetAll();
+            
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual(2, list.head.value);
+            Assert.AreEqual(3, list.tail.value);
+            Assert.AreEqual(null, list.tail.next);
+        }
+        
+        [Test]
+        public void DeleteDesc_ValueInTail_Deleted()
+        {
+            var list = new OrderedList<int>(false);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+
+            list.Delete(4);
+            var result = list.GetAll();
+            
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual(3, list.head.value);
+            Assert.AreEqual(2, list.tail.value);
+            Assert.AreEqual(null, list.tail.next);
+        }
+        
+        [Test]
+        public void Delete_Middle_Deleted()
+        {
+            var list = new OrderedList<int>(true);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+
+            list.Delete(3);
+            var result = list.GetAll();
+            
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual(2, list.head.value);
+            Assert.AreEqual(4, list.tail.value);
+            Assert.AreEqual(null, list.tail.next);
+            Assert.AreEqual(2, list.tail.prev.value);
+        }
+        
+        [Test]
+        public void DeleteDesc_Middle_Deleted()
+        {
+            var list = new OrderedList<int>(false);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+
+            list.Delete(3);
+            var result = list.GetAll();
+            
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual(4, list.head.value);
+            Assert.AreEqual(2, list.tail.value);
+            Assert.AreEqual(null, list.tail.next);
+            Assert.AreEqual(4, list.tail.prev.value);
+        }
+        
+        [Test]
+        public void Delete_Middle2_Deleted()
+        {
+            var list = new OrderedList<int>(true);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            list.Add(2);
+
+            list.Delete(4);
+            var result = list.GetAll();
+
+            var expected = new List<int>
+            {
+                2, 3, 5
+            };
+            
+            CollectionAssert.AreEqual(expected, result.Select(x => x.value).ToList());
+            Assert.AreEqual(5, list.tail.value);
+            Assert.AreEqual(null, list.tail.next);
+            Assert.AreEqual(3, list.tail.prev.value);
+            Assert.That(list.head.value, Is.EqualTo(2));
+        }
+        
+        [Test]
+        public void DeleteDesc_Middle2_Deleted()
+        {
+            var list = new OrderedList<int>(false);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            list.Add(2);
+
+            list.Delete(4);
+            var result = list.GetAll();
+
+            var expected = new List<int>
+            {
+                5,3,2
+            };
+            
+            CollectionAssert.AreEqual(expected, result.Select(x => x.value).ToList());
+            Assert.AreEqual(2, list.tail.value);
+            Assert.AreEqual(null, list.tail.next);
+            Assert.AreEqual(3, list.tail.prev.value);
+            Assert.That(list.head.value, Is.EqualTo(5));
+        }
+
+        #endregion
+
+        #region Find
+
+        [Test]
+        public void Find_Empty()
+        {
+            var list = new OrderedList<int>(true);
+
+            var res = list.Find(5);
+            
+            Assert.IsNull(res);
+        }
+
+        [Test]
+        [TestCase(true)]
+        [TestCase(false)]
+        public void FindSingle(bool asc)
+        {
+            var list = new OrderedList<int>(asc);
+            list.Add(1);
+
+            var res = list.Find(1);
+            Assert.That(res.value, Is.EqualTo(1));
+            
+            Assert.IsNull(list.Find(5));
+        }
+
+        [Test]
+        [TestCase(true)]
+        [TestCase(false)]
+        public void Find(bool asc)
+        {
+            var list = new OrderedList<int>(asc);
+            list.Add(5);
+            list.Add(2);
+            list.Add(3);
+            list.Add(1);
+            list.Add(4);
+
+            var res = list.Find(2);
+            
+            Assert.That(res.value, Is.EqualTo(2));
+            Assert.IsNull(list.Find(6));
+        }
+        
+        [Test]
+        [TestCase(true, 5, 5)]
+        [TestCase(false, 5, 5)]
+        [TestCase(true, 2, 2)]
+        [TestCase(true, 3, 3)]
+        [TestCase(true, 7, 7)]
+        [TestCase(true, 9, 9)]
+        [TestCase(true, 11, 11)]
+        [TestCase(true, 12, 12)]
+        [TestCase(true, 15, 15)]
+        [TestCase(true, 1, 1)]
+        [TestCase(false, 2, 2)]
+        [TestCase(false, 3, 3)]
+        [TestCase(false, 7, 7)]
+        [TestCase(false, 9, 9)]
+        [TestCase(false, 11, 11)]
+        [TestCase(false, 12, 12)]
+        [TestCase(false, 15, 15)]
+        [TestCase(false, 1, 1)]
+        [TestCase(false, -1, null)]
+        [TestCase(true, -1, null)]
+        [TestCase(true, 20, null)]
+        [TestCase(false, 20, null)]
+        [TestCase(false, 8, null)]
+        [TestCase(true, 8, null)]
+        public void Find2(bool asc, int val, int? expected)
+        {
+            var list = new OrderedList<int>(asc);
+            list.Add(5);
+            list.Add(2);
+            list.Add(3);
+            list.Add(1);
+            list.Add(4);
+            list.Add(15);
+            list.Add(7);
+            list.Add(14);
+            list.Add(9);
+            list.Add(12);
+            list.Add(13);
+            list.Add(10);
+            list.Add(11);
+
+            var res = list.Find(val);
+
+            if (res == null)
+            {
+                Assert.IsNull(expected);
+            }
+            else
+            {
+                Assert.That(res.value, Is.EqualTo(expected.Value));
+            }
+        }
+        
+        #endregion
+    }
+}
