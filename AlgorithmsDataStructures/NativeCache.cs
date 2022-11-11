051f9bc53e8b5b8ed4d2ca3c9befc0b1e027063c
Konstantin.Varlamov <Konstantin.Varlamov@norbit.ru>
2022-11-11 09:42:44 +0300

    NativeCache

---
+++ AlgorithmsDataStructures/NativeCache.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+    public class NativeCache<T>
+    {
+        private const int step = 1;
+        private const int maxCircles = 1;
+        
+        private int size;
+        public string[] slots;
+        public T [] values;
+        public int [] hits;
+
+        public NativeCache(int size)
+        {
+            this.size = size;
+            slots = new string[size];
+            values = new T[size];
+            hits = new int[size];
+        }
+        
+
+        public T AddOrGetExisting(string key, T value)
+        {
+            int index = HashFun(key);
+            int seekIndex = Seek(index, key);
+            
+            //if we've had a key
+            if (seekIndex != -1)
+            {
+                hits[seekIndex]++;
+                return values[seekIndex];
+            }
+
+            
+            int newIndex = Seek(index);
+            
+            // if there no empty slot - change slot with minimum hits
+            if (newIndex == -1)
+            {
+                var minHits = FindMinHits();
+                hits[minHits] = 1;
+                slots[minHits] = key;
+                values[minHits] = value;
+                return value;
+            }
+            
+            //set new key-value to empty slot
+            slots[newIndex] = key;
+            values[newIndex] = value;
+            hits[newIndex]++;
+            return value;
+        }
+
+        private int FindMinHits()
+        {
+            int min = 0;
+            for (int i = 1; i < hits.Length; i++)
+            {
+                //the lowest - not need to go througth the all array
+                if (hits[min] == 0)
+                    return min;
+                
+                if (hits[i] < hits[min])
+                    min = i;
+            }
+
+            return min;
+        }
+
+        private int Seek(int index, string key = null)
+        {
+            int initialIndex = index;
+            
+            for (int j = 0; j <= maxCircles; j++)
+            {
+                if (slots[index] == key)
+                    return index;
+
+                int barrier = size;
+                if (j == maxCircles)
+                    barrier = initialIndex;
+                
+                for (int i = index + step; i < barrier; i+=step)
+                {
+                    if (slots[i] == key)
+                        return i;
+                }
+
+                index = 0;
+            }
+            
+            return -1;
+        }
+        private int HashFun(string key)
+        {
+            byte[] data = System.Text.Encoding.UTF8.GetBytes(key);
+            int sum = 0;
+            for (int i = 0; i < data.Length; i++)
+            {
+                sum += data[i];
+            }
+
+            return sum % size;
+        }
+        
+    }
+}
---
+++ Tests/AlgorithmsDataStructuresTests/NativeCacheTests.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using AlgorithmsDataStructures;
+using NUnit.Framework;
+
+namespace AlgorithmsDataStructuresTests
+{
+    public class NativeCacheTests
+    {
+        [Test]
+        public void GetOrSet_New_EmptySlotExists_OK()
+        {
+            var cache = new NativeCache<string>(5);
+            cache.AddOrGetExisting("1", "one");
+            cache.AddOrGetExisting("2", "two");
+            cache.AddOrGetExisting("3", "three");
+            cache.AddOrGetExisting("4", "four");
+            cache.AddOrGetExisting("5", "five");
+
+            var keys = new List<string>
+            {
+                "1", "2", "3", "4", "5"
+            };
+            
+            var values = new List<string>
+            {
+                "one", "two", "three", "four", "five"
+            };
+
+            foreach (var key in keys)
+            {
+                Assert.IsNotNull(cache.slots.FirstOrDefault(x => x.Equals(key)));
+            }
+            
+            foreach (var val in values)
+            {
+                Assert.IsNotNull(cache.values.FirstOrDefault(x => x.Equals(val)));
+            }
+
+            foreach (var hit in cache.hits)
+            {
+                Assert.That(hit, Is.EqualTo(1));
+            }
+        }
+        
+        [Test]
+        public void GetOrSet_New_EmptySlotNotExists_ChangeMinimum()
+        {
+            var cache = new NativeCache<string>(5);
+            cache.AddOrGetExisting("1", "one");
+            cache.AddOrGetExisting("2", "two");
+            cache.AddOrGetExisting("3", "three");
+            cache.AddOrGetExisting("4", "four");
+            cache.AddOrGetExisting("5", "five");
+            cache.AddOrGetExisting("1", "one");
+            cache.AddOrGetExisting("2", "two");
+            cache.AddOrGetExisting("3", "three");
+            cache.AddOrGetExisting("4", "four");
+            var res = cache.AddOrGetExisting("6", "six");
+
+            var keys = new List<string>
+            {
+                "1", "2", "3", "4", "6"
+            };
+            
+            var values = new List<string>
+            {
+                "one", "two", "three", "four", "six"
+            };
+
+            foreach (var key in keys)
+            {
+                Assert.That(cache.slots.FirstOrDefault(x => x.Equals(key)), Is.EqualTo(key));
+            }
+            
+            foreach (var val in values)
+            {
+                Assert.IsNotNull(cache.values.FirstOrDefault(x => x.Equals(val)));
+            }
+        }
+        
+        [Test]
+        public void GetOrSet_New_HadAKey_ReturnValyue()
+        {
+            var cache = new NativeCache<string>(5);
+            var res = cache.AddOrGetExisting("1", "one");
+
+            Assert.That(res, Is.EqualTo("one"));
+        }
+
+        [Test]
+        public void Hits()
+        {
+            var cache = new NativeCache<string>(5);
+            cache.AddOrGetExisting("1", "one");
+            cache.AddOrGetExisting("2", "two");
+            cache.AddOrGetExisting("2", "two");
+            cache.AddOrGetExisting("3", "three");
+            cache.AddOrGetExisting("3", "three");
+            cache.AddOrGetExisting("3", "three");
+            cache.AddOrGetExisting("4", "four");
+            cache.AddOrGetExisting("4", "four");
+            cache.AddOrGetExisting("4", "four");
+            cache.AddOrGetExisting("4", "four");
+            cache.AddOrGetExisting("5", "five");
+            cache.AddOrGetExisting("5", "five");
+            cache.AddOrGetExisting("5", "five");
+            cache.AddOrGetExisting("5", "five");
+            cache.AddOrGetExisting("5", "five");
+
+            var indx = Array.IndexOf(cache.hits, 1);
+            Assert.That(cache.slots[indx], Is.EqualTo("1"));
+            Assert.That(cache.values[indx], Is.EqualTo("one"));
+            
+            indx = Array.IndexOf(cache.hits, 2);
+            Assert.That(cache.slots[indx], Is.EqualTo("2"));
+            Assert.That(cache.values[indx], Is.EqualTo("two"));
+            
+            indx = Array.IndexOf(cache.hits, 3);
+            Assert.That(cache.slots[indx], Is.EqualTo("3"));
+            Assert.That(cache.values[indx], Is.EqualTo("three"));
+            
+            indx = Array.IndexOf(cache.hits, 4);
+            Assert.That(cache.slots[indx], Is.EqualTo("4"));
+            Assert.That(cache.values[indx], Is.EqualTo("four"));
+            
+            indx = Array.IndexOf(cache.hits, 5);
+            Assert.That(cache.slots[indx], Is.EqualTo("5"));
+            Assert.That(cache.values[indx], Is.EqualTo("five"));
+
+            cache.AddOrGetExisting("6", "six");
+            cache.AddOrGetExisting("6", "six");
+            cache.AddOrGetExisting("6", "six");
+            cache.AddOrGetExisting("6", "six");
+            cache.AddOrGetExisting("6", "six");
+            cache.AddOrGetExisting("6", "six");
+            
+            cache.AddOrGetExisting("7", "seven");
+            cache.AddOrGetExisting("7", "seven");
+            cache.AddOrGetExisting("7", "seven");
+            cache.AddOrGetExisting("7", "seven");
+            cache.AddOrGetExisting("7", "seven");
+            cache.AddOrGetExisting("7", "seven");
+            cache.AddOrGetExisting("7", "seven");
+            
+
+            cache.AddOrGetExisting("8", "eight");
+            cache.AddOrGetExisting("8", "eight");
+            cache.AddOrGetExisting("8", "eight");
+            cache.AddOrGetExisting("8", "eight");
+            cache.AddOrGetExisting("8", "eight");
+            cache.AddOrGetExisting("8", "eight");
+            cache.AddOrGetExisting("8", "eight");
+            cache.AddOrGetExisting("8", "eight");
+            
+            cache.AddOrGetExisting("9", "nine");
+            cache.AddOrGetExisting("9", "nine");
+            cache.AddOrGetExisting("9", "nine");
+            cache.AddOrGetExisting("9", "nine");
+            cache.AddOrGetExisting("9", "nine");
+            cache.AddOrGetExisting("9", "nine");
+            cache.AddOrGetExisting("9", "nine");
+            cache.AddOrGetExisting("9", "nine");
+            cache.AddOrGetExisting("9", "nine");
+            
+            cache.AddOrGetExisting("10", "ten");
+            cache.AddOrGetExisting("10", "ten");
+            cache.AddOrGetExisting("10", "ten");
+            cache.AddOrGetExisting("10", "ten");
+            cache.AddOrGetExisting("10", "ten");
+            cache.AddOrGetExisting("10", "ten");
+            cache.AddOrGetExisting("10", "ten");
+            cache.AddOrGetExisting("10", "ten");
+            cache.AddOrGetExisting("10", "ten");
+            cache.AddOrGetExisting("10", "ten");
+            
+            indx = Array.IndexOf(cache.hits, 6);
+            Assert.That(cache.slots[indx], Is.EqualTo("6"));
+            Assert.That(cache.values[indx], Is.EqualTo("six"));
+            
+            indx = Array.IndexOf(cache.hits, 7);
+            Assert.That(cache.slots[indx], Is.EqualTo("7"));
+            Assert.That(cache.values[indx], Is.EqualTo("seven"));
+            
+            indx = Array.IndexOf(cache.hits, 8);
+            Assert.That(cache.slots[indx], Is.EqualTo("8"));
+            Assert.That(cache.values[indx], Is.EqualTo("eight"));
+            
+            indx = Array.IndexOf(cache.hits, 9);
+            Assert.That(cache.slots[indx], Is.EqualTo("9"));
+            Assert.That(cache.values[indx], Is.EqualTo("nine"));
+            
+            indx = Array.IndexOf(cache.hits, 10);
+            Assert.That(cache.slots[indx], Is.EqualTo("10"));
+            Assert.That(cache.values[indx], Is.EqualTo("ten"));
+        }
+    }
+}
