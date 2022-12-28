8e5611aa77398a74adb84d0f9c7b4bf5465d6d9b
Konstantin.Varlamov <k.varlamov88@yandex.ru>
2022-12-28 22:19:25 +0300

    FindSecondMax

---
+++ Recursion/FindSecondMax.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Recursion
+{
+    public class FindSecondMax
+    {
+        public static int Run(List<int> list)
+        {
+            if (list == null || list.Count == 0)
+                throw new ArgumentException("Wrong argument");
+            
+            int max = list.Max();
+
+            return GetSecondMax(list, max, 0);
+        }
+        
+        private static int GetSecondMax(List<int> list, int max, int curIndex, bool isSecond = false)
+        {
+            // if not found
+            if (curIndex > list.Count - 1)
+                return -1;
+
+            // return second max
+            if (isSecond && list[curIndex] == max)
+                return list[curIndex];
+            
+            // find first max, set flag to true
+            if (list[curIndex] == max)
+                isSecond = true;
+
+            return GetSecondMax(list, max, curIndex + 1, isSecond);
+        }
+    }
+}
---
+++ Tests/RecursionTests/FindSecondMaxTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Recursion;
+
+namespace RecursionTests
+{
+    [TestFixture]
+    public class FindSecondMaxTests
+    {
+        [Test, TestCaseSource(nameof(GetData))]
+        public void Test(List<int> list, int expected)
+        {
+            Assert.That(FindSecondMax.Run(list), Is.EqualTo(expected));
+        }
+
+        private static IEnumerable<TestCaseData> GetData()
+        {
+            yield return new TestCaseData(new List<int>(){ 5,4,3,2,5 }, 5);
+            yield return new TestCaseData(new List<int>(){ 2,4,2,2,4,3 }, 4);
+            yield return new TestCaseData(new List<int>(){ 2,4,5,2,4,3 }, -1);
+            yield return new TestCaseData(new List<int>(){ 2,2,2,3 }, -1);
+            yield return new TestCaseData(new List<int>(){ 2,4,2,3, 6, 1, 2,6, 5 }, 6);
+        }
+    }
+}
