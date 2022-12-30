da564a7e3ade1d0d66c42b7324e4aa2540fd64b5
Konstantin.Varlamov <k.varlamov88@yandex.ru>
2022-12-30 16:47:15 +0300

    added tests

--- Tests/RecursionTests/FindSecondMaxTests.cs
+++ Tests/RecursionTests/FindSecondMaxTests.cs
@@ -36,7 +36,9 @@ namespace RecursionTests
             yield return new TestCaseData(new List<int>() {5,2,1}, 2);
             yield return new TestCaseData(new List<int>() {5,1, 5}, 5);
             yield return new TestCaseData(new List<int>() {5,1}, 1);
-
+            yield return new TestCaseData(new List<int>() {5,4, 4, 3}, 4);
+            yield return new TestCaseData(new List<int>() {5,4, 4, 6}, 5);
+            yield return new TestCaseData(new List<int>() {5,4, 6, 6}, 6);
         }
     }
 }
