293fed5997a134d6e5ea2c12021d253af30a3221
Konstantin.Varlamov <k.varlamov88@yandex.ru>
2022-12-30 16:35:58 +0300

    Fixes

--- Recursion/FindSecondMax.cs
+++ Recursion/FindSecondMax.cs
@@ -7,51 +7,58 @@ namespace Recursion
     {
         public static int Run(List<int> list)
         {
-            if (list == null || list.Count == 0)
+            if (list == null || list.Count == 0 || list.Count < 2)
                 throw new ArgumentException("Wrong argument");
 
-            return GetSecondMax(list);
+            int max, secondMax;
+            if (list[0] >= list[1])
+            {
+                max = list[0];
+                secondMax = list[1];
+            }
+            else
+            {
+                max = list[1];
+                secondMax = list[0];
+            }
+            
+            return GetSecondMax(list, 2, max, secondMax);
+            // return GetSecondMaxNoRec(list, max, secondMax);
         }
         
-        private static int GetSecondMax(List<int> list, int i = 0, int maxFirst = -1, int maxSecond = -1)
+        private static int GetSecondMax(List<int> list, int i, int max, int secondMax)
         {
             //if list finished
             if (i > list.Count - 1)
-                return maxSecond;
+                return secondMax;
 
-            if (list[i] == maxFirst)
-                maxSecond = maxFirst;
-            
-            if (list[i] > maxFirst)
+            if (list[i] > max )
             {
-                maxSecond = -1;
-                maxFirst = list[i];
+                secondMax = max;
+                max = list[i];
+            } else if (list[i] > secondMax)
+            {
+                secondMax = list[i];
             }
 
-            return GetSecondMax(list, i + 1, maxFirst, maxSecond);
+            return GetSecondMax(list, i + 1, max, secondMax);
         }
 
-        private static int GetSecondMaxNoRec(List<int> list)
+        private static int GetSecondMaxNoRec(List<int> list, int max, int secondMax)
         {
-            int maxFirst = -1;
-            int maxSecond = -1;
-            
-            for (var i = 0; i < list.Count; i++)
+            for (var i = 2; i < list.Count; i++)
             {
-                if (list[i] < maxFirst)
-                    continue;
-                
-                if (list[i] == maxFirst)
-                    maxSecond = maxFirst;
-                
-                if (list[i] > maxFirst)
+                if (list[i] > max )
                 {
-                    maxSecond = -1;
-                    maxFirst = list[i];
+                    secondMax = max;
+                    max = list[i];
+                } else if (list[i] > secondMax)
+                {
+                    secondMax = list[i];
                 }
             }
             
-            return maxSecond;
+            return secondMax;
         }
     }
 }
--- Tests/RecursionTests/FindSecondMaxTests.cs
+++ Tests/RecursionTests/FindSecondMaxTests.cs
@@ -17,10 +17,20 @@ namespace RecursionTests
         {
             yield return new TestCaseData(new List<int>(){ 5,4,3,2,5 }, 5);
             yield return new TestCaseData(new List<int>(){ 2,4,2,2,4,3 }, 4);
-            yield return new TestCaseData(new List<int>(){ 2,4,5,2,4,3 }, -1);
-            yield return new TestCaseData(new List<int>(){ 2,2,2,3 }, -1);
+            yield return new TestCaseData(new List<int>(){ 2,4,5,2,4,3 }, 4);
+            yield return new TestCaseData(new List<int>(){ 2,2,2,3 }, 2);
             yield return new TestCaseData(new List<int>(){ 2,4,2,3, 6, 1, 2,6, 5 }, 6);
             yield return new TestCaseData(new List<int>() {0, 0, 0, 0, 0, 0}, 0);
+            yield return new TestCaseData(new List<int>() {1, 2, 2, 3, 4}, 3);
+            yield return new TestCaseData(new List<int>() {1, 2}, 1);
+            yield return new TestCaseData(new List<int>() {2, 1}, 1);
+            yield return new TestCaseData(new List<int>() {1, 1}, 1);
+            yield return new TestCaseData(new List<int>() {-1000, -10000, 5, 31, 8, -1500}, 8);
+            yield return new TestCaseData(new List<int>() {-1000, -10000, int.MinValue, -1500}, -1500);
+            yield return new TestCaseData(new List<int>() {int.MinValue, int.MinValue}, int.MinValue);
+            yield return new TestCaseData(new List<int>() {int.MinValue, 1}, int.MinValue);
+            yield return new TestCaseData(new List<int>() {int.MaxValue, 1}, 1);
+            
         }
     }
 }
