0504f4a7a7f4c4d91937bb44de557f2d55e2a17b
Konstantin.Varlamov <k.varlamov88@yandex.ru>
2023-01-14 10:04:46 +0300

    aBST

---
+++ AlgorithmsDataStructures2/aBST.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures2
+{
+    public class aBST
+    {
+        public int? [] Tree; // массив ключей
+	
+        public aBST(int depth)
+        {
+            int tree_size = GetTreeSizeByDepth(depth);
+            Tree = new int?[tree_size];
+            for(int i=0; i<tree_size; i++) 
+                Tree[i] = null;
+        }
+	
+        public int? FindKeyIndex(int key)
+        {
+            // ищем в массиве индекс ключа
+            if (Tree == null || (Tree.Length == 1 && Tree[0] == null))
+                return null;
+
+            return FindKeyIndex(0, key);
+        }
+	
+        public int AddKey(int key)
+        {
+            // добавляем ключ в массив
+            // индекс добавленного/существующего ключа или -1 если не удалось
+            var findResult = FindKeyIndex(key);
+            
+            if (findResult == null)
+                return -1;
+            
+            if (findResult >= 0)
+                return findResult.Value;
+
+            int newIndex = Math.Abs(findResult.Value); 
+            Tree[newIndex] = key;
+            return newIndex;
+        }
+
+        private int GetTreeSizeByDepth(int depth)
+        {
+            if (depth == 0)
+                return 1;
+
+            return (int)(Math.Pow(2, depth + 1) - 1);
+        }
+
+        private int? FindKeyIndex(int currentIndex, int key)
+        {
+            if (currentIndex > Tree.Length - 1)
+                return null;
+
+            var currentKey = Tree[currentIndex];
+            if (currentKey == key)
+                return currentIndex;
+
+            if (currentKey is null)
+                return currentIndex * -1;
+
+            if (currentKey > key)
+                //ищем в левом поддереве
+                return FindKeyIndex(2 * currentIndex + 1, key);
+            else
+                //ищем в правом поддереве
+                return FindKeyIndex(2 * currentIndex + 2, key);
+        }
+    }
+}
---
+++ Tests/AlgorithmDataStructures2Tests/aBSTTests.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections.Generic;
+using AlgorithmsDataStructures2;
+using NUnit.Framework;
+
+namespace AlgorithmDataStructures2Tests
+{
+    [TestFixture]
+    public class aBSTTests
+    {
+        #region Find
+
+        [Test]
+        public void FindInEmptyTree()
+        {
+            var tree = new aBST(0);
+            
+            Assert.IsNull(tree.FindKeyIndex(1));
+        }
+        
+        [Test]
+        public void FindOnlyOneTree()
+        {
+            var tree = new aBST(0);
+            tree.Tree[0] = 1;
+            
+            Assert.That(tree.FindKeyIndex(1), Is.EqualTo(0));
+        }
+
+        [Test]
+        [TestCase(100)]
+        [TestCase(49)]
+        [TestCase(54)]
+        [TestCase(30)]
+        public void NotFind(int key)
+        {
+            var tree = new aBST(3);
+            FillTree(tree.Tree);
+
+            var res = tree.FindKeyIndex(key);
+            
+            Assert.IsNull(res);
+        }
+
+        [Test]
+        [TestCase(50, 0)]
+        [TestCase(25, 1)]
+        [TestCase(37,4)]
+        [TestCase(75,2)]
+        [TestCase(62,5)]
+        [TestCase(84,6)]
+        [TestCase(31,9)]
+        [TestCase(43,10)]
+        [TestCase(55,11)]
+        [TestCase(92,14)]
+        public void FindNormal(int key, int expectedIndex)
+        {
+            var tree = new aBST(3);
+            FillTree(tree.Tree);
+
+            var resultIndex = tree.FindKeyIndex(key);
+            Assert.That(resultIndex, Is.EqualTo(expectedIndex));
+        }
+
+        [Test]
+        [TestCase(24, -3)]
+        [TestCase(64, -12)]
+        [TestCase(80, -13)]
+        public void FindEmptySlot(int key, int expectedIndex)
+        {
+            var tree = new aBST(3);
+            FillTree(tree.Tree);
+
+            var resultIndex = tree.FindKeyIndex(key);
+            Assert.That(resultIndex, Is.EqualTo(expectedIndex));
+        }
+
+        #endregion
+
+        #region Add
+
+        [Test]
+        public void AddEmpty()
+        {
+            
+        }
+
+        [Test]
+        [TestCase(50, 0)]
+        [TestCase(25, 1)]
+        [TestCase(37,4)]
+        [TestCase(75,2)]
+        [TestCase(62,5)]
+        [TestCase(84,6)]
+        [TestCase(31,9)]
+        [TestCase(43,10)]
+        [TestCase(55,11)]
+        [TestCase(92,14)]
+        public void AddExisting(int key, int index)
+        {
+            var tree = new aBST(3);
+            FillTree(tree.Tree);
+
+            var res = tree.AddKey(key);
+            
+            Assert.That(res, Is.EqualTo(index));
+        }
+
+        [Test]
+        [TestCase(24, 3)]
+        [TestCase(64, 12)]
+        [TestCase(80, 13)]
+        public void AddNormal(int key, int index)
+        {
+            var tree = new aBST(3);
+            FillTree(tree.Tree);
+
+            var res = tree.AddKey(key);
+            
+            Assert.That(res, Is.EqualTo(index));
+        }
+
+        #endregion
+        
+        private void FillTree(int?[] tree)
+        {
+            var list = new List<int?>()
+            {
+                50, 25, 75, null, 37, 62, 84, null, null, 31, 43, 55, null, null, 92
+            };
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                tree[i] = list[i];
+            }
+        }
+    }
+}
