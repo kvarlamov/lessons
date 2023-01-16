bbc7fc20c68b7da0eb2915d43e04a65ca70a5bed
Konstantin.Varlamov <k.varlamov88@yandex.ru>
2023-01-16 17:29:29 +0300

    Generate aBST function

---
+++ AlgorithmsDataStructures2/BalancedBST.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures2
+{
+    public static class BalancedBST
+    {
+        public static int[] GenerateBBSTArray(int[] a)
+        {
+            if (a== null || a.Length == 0)
+                return null;
+            
+            //sort input array
+            Array.Sort(a);
+
+            var aBst = new int[a.Length];
+            
+            G(a, aBst, 0, 0, a.Length - 1);
+
+            return aBst;
+        }
+
+        //i - current index in new array
+        private static void G(int[] input, int[] aBST, int i,  int left, int right)
+        {
+            if (left > right || i > aBST.Length - 1)
+                return;
+            
+            int rootIndex = GetCentralIndex(left, right);
+            aBST[i] = input[rootIndex];
+            
+            // set left
+            G(input, aBST, 2 * i + 1, left, rootIndex - 1);
+
+            // set right
+            G(input, aBST, 2 * i + 2, rootIndex + 1, right);
+        }
+        
+        private static int GetCentralIndex(int left, int right) => (left + right) / 2;
+    }
+}
---
+++ Tests/AlgorithmDataStructures2Tests/BalancedBSTTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using AlgorithmsDataStructures2;
+using NUnit.Framework;
+
+namespace AlgorithmDataStructures2Tests
+{
+    [TestFixture]
+    public class BalancedBSTTests
+    {
+        [Test]
+        public void EmptyArr()
+        {
+            Assert.IsNull(BalancedBST.GenerateBBSTArray(null));
+        }
+
+        [Test]
+        public void OneElementArray()
+        {
+            var arrayInitial = new int[] {8};
+            var copy = new int[arrayInitial.Length];
+            Array.Copy(arrayInitial, copy, arrayInitial.Length);
+            
+            var res = BalancedBST.GenerateBBSTArray(copy);
+            
+            CollectionAssert.AreEqual(arrayInitial, res);
+        }
+
+        [Test]
+        public void TheeElemArray()
+        {
+            var arrayInitial = new int[] {8, 4, 12};
+            var copy = new int[arrayInitial.Length];
+            Array.Copy(arrayInitial, copy, arrayInitial.Length);
+            
+            var res = BalancedBST.GenerateBBSTArray(copy);
+            
+            CollectionAssert.AreEqual(arrayInitial, res);
+        }
+        
+        [Test]
+        public void t1()
+        {
+            var arrayInitial = new int[] {50, 25, 75, 20, 37, 62, 84, 10, 22, 31, 43, 55, 64, 83, 92};
+            var copy = new int[arrayInitial.Length];
+            Array.Copy(arrayInitial, copy, arrayInitial.Length);
+
+            var res = BalancedBST.GenerateBBSTArray(copy);
+            
+            CollectionAssert.AreEqual(arrayInitial, res);
+        }
+
+        [Test]
+        public void t2()
+        {
+            var arrayInitial = new int[] {8,4,12,2,6,10,14,1,3,5,7,9,11,13,15};
+            var copy = new int[arrayInitial.Length];
+            Array.Copy(arrayInitial, copy, arrayInitial.Length);
+
+            var res = BalancedBST.GenerateBBSTArray(copy);
+            
+            CollectionAssert.AreEqual(arrayInitial, res);
+        }
+        
+        [Test]
+        public void t3()
+        {
+            var arrayInitial = new int[] {8,4,12,2,6,10,14};
+            var copy = new int[arrayInitial.Length];
+            Array.Copy(arrayInitial, copy, arrayInitial.Length);
+
+            var res = BalancedBST.GenerateBBSTArray(copy);
+            
+            CollectionAssert.AreEqual(arrayInitial, res);
+        }
+    }
+}
