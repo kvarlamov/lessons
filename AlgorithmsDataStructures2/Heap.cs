ca041b29efdae9157b70897446979901209c12be
Konstantin.Varlamov <k.varlamov88@yandex.ru>
2023-01-21 18:50:30 +0300

    Heap

---
+++ AlgorithmsDataStructures2/Heap.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures2
+{
+    public class Heap
+    {
+        public int [] HeapArray; // хранит неотрицательные числа-ключи
+
+        public int lastPointer; // указатель на свободный слот (если куча заполнена = длине HeapArray)
+
+        public Heap()
+        {
+            HeapArray = null;
+            lastPointer = 0;
+        }
+		
+        public void MakeHeap(int[] a, int depth)
+        {
+            // создаём массив кучи HeapArray из заданного
+            // размер массива выбираем на основе глубины depth
+            int size = GetTreeSizeByDepth(depth);
+            HeapArray = new int[size];
+            foreach (var node in a) 
+                Add(node);
+        }
+
+        public int GetMax()
+        {
+            // вернуть значение корня и перестроить кучу
+            if (HeapArray == null || HeapArray.Length == 0)
+                return -1; // если куча пуста
+
+            // сохраняем корень перед перемещением
+            int max = HeapArray[0];
+            // ставим на место корня последний элемент
+            HeapArray[0] = HeapArray[lastPointer-1];
+            lastPointer--;
+
+            // обновляем новый корень
+            MoveDown(0);
+
+            return max;
+        }
+
+        public bool Add(int key)
+        {
+            // добавляем новый элемент key в кучу и перестраиваем её
+            if (lastPointer == HeapArray.Length)
+                return false; // если куча вся заполнена
+
+            Insert(key);
+            lastPointer++;
+            return true;
+        }
+
+        /// <summary>
+        /// Операция просеивания вниз
+        /// </summary>
+        /// <param name="i"></param>
+        private void MoveDown(int i)
+        {
+            while (true)
+            {
+                int left = 2 * i + 1;
+                int right = 2 * i + 2;
+                int max = i;
+                
+                // если левый больше максимума делаем его максимумом
+                if (HeapArray[left] > HeapArray[max] && left < lastPointer)
+                    max = left;
+                
+                // если правый больше максимума делаем его максимумом
+                if (HeapArray[right] > HeapArray[max] && right < lastPointer)
+                    max = right;
+                
+                // останавливаемся, когда у родителя будет больший ключ, а у двух наследников -- меньшие.
+                if (max == i)
+                    break;
+
+                (HeapArray[max], HeapArray[i]) = (HeapArray[i], HeapArray[max]);
+                i = max;
+            }
+        }
+        
+        private void Insert(int key)
+        {
+            // находим индекс родителя
+            HeapArray[lastPointer] = key;
+            int i = lastPointer;
+            int parent = (i - 1) / 2;
+
+            while (i > 0 && HeapArray[parent] < HeapArray[i])
+            {
+                (HeapArray[i], HeapArray[parent]) = (HeapArray[parent], HeapArray[i]);
+
+                i = parent;
+                parent = (i - 1) / 2;
+            }
+        }
+        
+        private int GetTreeSizeByDepth(int depth)
+        {
+            if (depth == 0)
+                return 1;
+
+            return (int)(Math.Pow(2, depth + 1) - 1);
+        }
+    }
+}
---
+++ Tests/AlgorithmDataStructures2Tests/HeapTests.cs
@@ -0,0 +1,181 @@
+﻿using System.Collections.Generic;
+using AlgorithmsDataStructures2;
+using NUnit.Framework;
+
+namespace AlgorithmDataStructures2Tests
+{
+    [TestFixture]
+    public class HeapTests
+    {
+        #region MakeHeap
+
+        [Test]
+        public void MakeHeap_MaxInTop()
+        {
+            Heap heap = new Heap()
+            {
+                HeapArray = new int[15]
+            };
+
+            List<int> initial = new List<int>()
+            {
+                9, 11, 6, 8, 7, 8, 1, 3, 5, 15
+            };
+            
+            heap.MakeHeap(initial.ToArray(), 3);
+            
+            Assert.That(heap.lastPointer, Is.EqualTo(10));
+            Assert.That(heap.HeapArray[0], Is.EqualTo(15));
+        }
+        
+        [Test]
+        public void MakeHeap_CorrectOrder()
+        {
+            Heap heap = new Heap()
+            {
+                HeapArray = new int[15]
+            };
+
+            List<int> initial = new List<int>()
+            {
+                15, 9, 11, 6, 8, 7, 8, 1, 3, 5 
+            };
+            
+            heap.MakeHeap(initial.ToArray(), 3);
+            
+            Assert.That(heap.lastPointer, Is.EqualTo(10));
+            Assert.That(heap.HeapArray[0], Is.EqualTo(15));
+            Assert.That(heap.HeapArray[1], Is.EqualTo(9));
+            Assert.That(heap.HeapArray[2], Is.EqualTo(11));
+            Assert.That(heap.HeapArray[3], Is.EqualTo(6));
+            Assert.That(heap.HeapArray[4], Is.EqualTo(8));
+            Assert.That(heap.HeapArray[5], Is.EqualTo(7));
+            Assert.That(heap.HeapArray[6], Is.EqualTo(8));
+            Assert.That(heap.HeapArray[7], Is.EqualTo(1));
+            Assert.That(heap.HeapArray[8], Is.EqualTo(3));
+            Assert.That(heap.HeapArray[9], Is.EqualTo(5));
+        }
+
+        #endregion
+        
+        #region Delete
+
+        [Test]
+        public void Delete_Empty()
+        {
+            Heap heap = new Heap();
+            
+            Assert.That(heap.GetMax(), Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void Delete_OnlyRoot()
+        {
+            Heap heap = new Heap()
+            {
+                HeapArray = new int[15]
+            };
+
+            heap.Add(20);
+            
+            Assert.That(heap.GetMax(), Is.EqualTo(20));
+            Assert.That(heap.lastPointer, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Delete_Partly()
+        {
+            Heap heap = new Heap()
+            {
+                HeapArray = new int[15]
+            };
+
+            heap.Add(15);
+            heap.Add(9);
+            heap.Add(11);
+            heap.Add(6);
+            heap.Add(8);
+            heap.Add(7);
+            heap.Add(8);
+            heap.Add(1);
+            heap.Add(3);
+            heap.Add(5);
+            
+            Assert.That(heap.GetMax(), Is.EqualTo(15));
+            Assert.That(heap.lastPointer, Is.EqualTo(9));
+            Assert.That(heap.HeapArray[0], Is.EqualTo(11));
+            Assert.That(heap.HeapArray[1], Is.EqualTo(9));
+            Assert.That(heap.HeapArray[2], Is.EqualTo(8));
+            Assert.That(heap.HeapArray[3], Is.EqualTo(6));
+            Assert.That(heap.HeapArray[5], Is.EqualTo(7));
+            Assert.That(heap.HeapArray[6], Is.EqualTo(5));
+            Assert.That(heap.HeapArray[8], Is.EqualTo(3));
+        }
+
+        #endregion
+
+        #region Add
+        
+        [Test]
+        public void Add_EmptyArray_True()
+        {
+            Heap heap = new Heap()
+            {
+                HeapArray = new int[15]
+            };
+
+            Assert.IsTrue(heap.Add(11));
+            
+            Assert.That(heap.HeapArray[0], Is.EqualTo(11));
+        }
+
+        [Test]
+        public void Add_EmptyAndFillAll_True()
+        {
+            Heap heap = new Heap()
+            {
+                HeapArray = new int[15]
+            };
+            
+            Assert.IsTrue(heap.Add(11));
+            Assert.That(heap.HeapArray[0], Is.EqualTo(11));
+            Assert.IsTrue(heap.Add(9));
+            Assert.That(heap.HeapArray[1], Is.EqualTo(9));
+            Assert.IsTrue(heap.Add(4));
+            Assert.That(heap.HeapArray[2], Is.EqualTo(4));
+        }
+
+        [Test]
+        public void Add_PartlyFilled_True()
+        {
+            Heap heap = new Heap
+            {
+                HeapArray = new int[15] {20, 15, 11, 6,9,7,8,1,3,5,0,0,0,0,0}
+            };
+
+            heap.lastPointer = 10;
+            
+            Assert.IsTrue(heap.Add(17));
+            Assert.That(heap.HeapArray[10], Is.EqualTo(9));
+            Assert.That(heap.HeapArray[4], Is.EqualTo(15));
+            Assert.That(heap.HeapArray[9], Is.EqualTo(5));
+            Assert.That(heap.HeapArray[1], Is.EqualTo(17));
+        }
+
+        [Test]
+        public void Add_AllFilled_False()
+        {
+            Heap heap = new Heap
+            {
+                HeapArray = new[] {11, 9, 4, 7, 8, 3, 1, 2, 5, 6, 5, 2, 1, 0, 0}
+            };
+
+            heap.lastPointer = 15;
+
+            Assert.IsFalse(heap.Add(10));
+        }
+
+        #endregion
+        
+    }
+}
