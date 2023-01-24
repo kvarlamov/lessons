25ab7e722a8c889e4d87d51c336e1ed8ed1ca7c7
Konstantin.Varlamov <k.varlamov88@yandex.ru>
2023-01-24 21:49:33 +0300

    SimpleGraph

---
+++ AlgorithmsDataStructures2/SimpleGraph.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures2
+{
+    public class Vertex
+    {
+        public int Value;
+        public Vertex(int val)
+        {
+            Value = val;
+        }
+    }
+  
+    public class SimpleGraph
+    {
+        public Vertex [] vertex;
+        public int [,] m_adjacency; // матрица смежности
+        public int max_vertex; // список, хранящий вершины (Индексы соответствуют индексам матрицы)
+	
+        public SimpleGraph(int size)
+        {
+            max_vertex = size;
+            m_adjacency = new int [size,size];
+            vertex = new Vertex [size];
+        }
+	
+        public void AddVertex(int value)
+        {
+            // код добавления новой вершины 
+            // с значением value 
+            // в свободную позицию массива vertex
+
+            for (int i = 0; i < vertex.Length; i++)
+            {
+                if (vertex[i] != null)
+                    continue;
+                
+                var newVertex = new Vertex(value);
+                vertex[i] = newVertex;
+            }
+        }
+
+        // здесь и далее, параметры v -- индекс вершины
+        // в списке  vertex
+        public void RemoveVertex(int v)
+        {
+            // ваш код удаления вершины со всеми её рёбрами
+            if (!IsIndexCorrect(v))
+                return;
+
+            vertex[v] = null;
+            for (int i = 0; i < vertex.Length; i++)
+            {
+                m_adjacency[v, i] = 0;
+            }
+        }
+	
+        public bool IsEdge(int v1, int v2)
+        {
+            // true если есть ребро между вершинами v1 и v2
+            return IsIndexCorrect(v1) && IsIndexCorrect(v2) && m_adjacency[v1, v2] == 1;
+        }
+	
+        public void AddEdge(int v1, int v2)
+        {
+            // добавление ребра между вершинами v1 и v2
+            if (!IsIndexCorrect(v1) || !IsIndexCorrect(v2))
+                return;
+
+            m_adjacency[v1, v2] = 1;
+        }
+	
+        public void RemoveEdge(int v1, int v2)
+        {
+            // удаление ребра между вершинами v1 и v2
+            if (!IsIndexCorrect(v1) || !IsIndexCorrect(v2))
+                return;
+            
+            m_adjacency[v1, v2] = 0;
+        }
+
+        private bool IsIndexCorrect(int i) =>
+            i >= 0 && i < vertex.Length;
+    }
+}
---
+++ Tests/AlgorithmDataStructures2Tests/SimpleGraphTests.cs
@@ -0,0 +1,201 @@
+﻿using System.Linq;
+using AlgorithmsDataStructures2;
+using NUnit.Framework;
+
+namespace AlgorithmDataStructures2Tests
+{
+    [TestFixture]
+    public class SimpleGraphTests
+    {
+        #region IsEdge
+
+        [Test]
+        public void IsEdge_Empty_False()
+        {
+            SimpleGraph graph = new SimpleGraph(5);
+            
+            Assert.IsFalse(graph.IsEdge(0, 1));
+        }
+
+        [Test]
+        public void IsEdge_OnlyOne_False()
+        {
+            SimpleGraph graph = new SimpleGraph(5);
+            graph.AddVertex(2);
+            
+            Assert.IsFalse(graph.IsEdge(0, 1));
+        }
+
+        [Test]
+        public void IsEdge_AllWithoutEdges_False()
+        {
+            SimpleGraph graph = new SimpleGraph(5);
+            graph.AddVertex(1);
+            graph.AddVertex(2);
+            graph.AddVertex(3);
+            graph.AddVertex(4);
+            graph.AddVertex(5);
+            
+            Assert.IsFalse(graph.IsEdge(0, 1));
+            Assert.IsFalse(graph.IsEdge(1, 1));
+            Assert.IsFalse(graph.IsEdge(2, 1));
+            Assert.IsFalse(graph.IsEdge(3, 1));
+            Assert.IsFalse(graph.IsEdge(4, 1));
+            
+            Assert.IsFalse(graph.IsEdge(0, 0));
+            Assert.IsFalse(graph.IsEdge(1, 0));
+            Assert.IsFalse(graph.IsEdge(2, 0));
+            Assert.IsFalse(graph.IsEdge(3, 0));
+            Assert.IsFalse(graph.IsEdge(4, 0));
+            
+            Assert.IsFalse(graph.IsEdge(0, 2));
+            Assert.IsFalse(graph.IsEdge(1, 2));
+            Assert.IsFalse(graph.IsEdge(2, 2));
+            Assert.IsFalse(graph.IsEdge(3, 2));
+            Assert.IsFalse(graph.IsEdge(4, 2));
+            
+            Assert.IsFalse(graph.IsEdge(0, 3));
+            Assert.IsFalse(graph.IsEdge(1, 3));
+            Assert.IsFalse(graph.IsEdge(2, 3));
+            Assert.IsFalse(graph.IsEdge(3, 3));
+            Assert.IsFalse(graph.IsEdge(4, 3));
+            
+            Assert.IsFalse(graph.IsEdge(0, 4));
+            Assert.IsFalse(graph.IsEdge(1, 4));
+            Assert.IsFalse(graph.IsEdge(2, 4));
+            Assert.IsFalse(graph.IsEdge(3, 4));
+            Assert.IsFalse(graph.IsEdge(4, 4));
+        }
+
+        [Test]
+        public void IsEdge_AllWithEdges_True()
+        {
+            SimpleGraph graph = new SimpleGraph(5);
+            graph.AddVertex(1);
+            graph.AddVertex(2);
+            graph.AddVertex(3);
+            graph.AddVertex(4);
+            graph.AddVertex(5);
+
+            graph.AddEdge(0, 1);
+            Assert.IsTrue(graph.IsEdge(0, 1));
+        }
+
+        #endregion
+        
+        #region AddEdge
+
+        [Test]
+        public void AddEdge_Correct()
+        {
+            // сначала связи нет
+            // потом она должна появиться
+            SimpleGraph graph = new SimpleGraph(5);
+            graph.AddVertex(1);
+            graph.AddVertex(2);
+            graph.AddVertex(3);
+            graph.AddVertex(4);
+            graph.AddVertex(5);
+
+            Assert.IsFalse(graph.IsEdge(0, 1));
+            graph.AddEdge(0, 1);
+            Assert.IsTrue(graph.IsEdge(0, 1));
+        }
+
+        #endregion
+        
+        #region RemoveEdge
+
+        [Test]
+        public void RemoveEdge_Correct()
+        {
+            // тест: до удаления связь между вершинами была, после удаления отсутствует);
+            SimpleGraph graph = new SimpleGraph(5);
+            graph.AddVertex(1);
+            graph.AddVertex(2);
+            graph.AddVertex(3);
+            graph.AddVertex(4);
+            graph.AddVertex(5);
+
+            graph.AddEdge(0, 1);
+            Assert.IsTrue(graph.IsEdge(0, 1));
+            graph.RemoveEdge(0, 1);
+            Assert.IsFalse(graph.IsEdge(0, 1));
+        }
+
+        #endregion
+        
+        #region AddVertex
+
+        [Test]
+        public void AddVertex_VertexFull_NotModified()
+        {
+            SimpleGraph graph = new SimpleGraph(5);
+            graph.AddVertex(1);
+            graph.AddVertex(2);
+            graph.AddVertex(3);
+            graph.AddVertex(4);
+            graph.AddVertex(5);
+            graph.AddVertex(6);
+            
+            Assert.IsFalse(graph.vertex.Any(x => x.Value == 6));
+        }
+
+        #endregion
+        
+        #region RemoveVertex
+
+        [Test]
+        public void RemoveVertex()
+        {
+            // (тест: до удаления некоторые вершины имеют связи с удаляемой вершиной, после удаления этих связей нету).
+            SimpleGraph graph = new SimpleGraph(5);
+            graph.AddVertex(1);
+            graph.AddVertex(2);
+            graph.AddVertex(3);
+            graph.AddVertex(4);
+            graph.AddVertex(5);
+            
+            graph.AddEdge(0,1);
+            graph.AddEdge(0,2);
+            graph.AddEdge(0,3);
+            graph.AddEdge(1,0);
+            graph.AddEdge(1,3);
+            graph.AddEdge(1,4);
+            graph.AddEdge(2,0);
+            graph.AddEdge(2,3);
+            graph.AddEdge(3,0);
+            graph.AddEdge(3,1);
+            graph.AddEdge(3,2);
+            graph.AddEdge(3,3);
+            graph.AddEdge(3,4);
+            graph.AddEdge(4,1);
+            graph.AddEdge(4,3);
+            
+            Assert.IsTrue(graph.IsEdge(0,1));
+            Assert.IsTrue(graph.IsEdge(0,2));
+            Assert.IsTrue(graph.IsEdge(0,3));
+            Assert.IsTrue(graph.IsEdge(1,0));
+            Assert.IsTrue(graph.IsEdge(1,3));
+            Assert.IsTrue(graph.IsEdge(1,4));
+            Assert.IsTrue(graph.IsEdge(2,0));
+            Assert.IsTrue(graph.IsEdge(2,3));
+            Assert.IsTrue(graph.IsEdge(3,0));
+            Assert.IsTrue(graph.IsEdge(3,1));
+            Assert.IsTrue(graph.IsEdge(3,2));
+            Assert.IsTrue(graph.IsEdge(3,3));
+            Assert.IsTrue(graph.IsEdge(3,4));
+            Assert.IsTrue(graph.IsEdge(4,1));
+            Assert.IsTrue(graph.IsEdge(4,3));
+            
+            graph.RemoveVertex(3);
+
+            for (int i = 0; i < 4; i++)
+            {
+                Assert.IsFalse(graph.IsEdge(3, i));
+            }
+        }
+
+        #endregion
+    }
+}
