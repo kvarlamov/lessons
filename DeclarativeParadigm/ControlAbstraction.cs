3dfb9e451ddac29c7b370e032864b2f4b78b0428
Konstantin.Varlamov <k.varlamov88@yandex.ru>
2023-07-04 21:59:36 +0300

    Declarative Paradigm. Control abstraction example

---
+++ DeclarativeParadigm/ControlAbstraction.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DeclarativeParadigm
+{
+    public interface IControlAbstraction<T>
+    {
+        T Iterate(T s, Func<T, bool> isDone, Func<T, T> transform);
+    }
+
+    public class ControlAbstraction<T> : IControlAbstraction<T>
+    {
+        public T Iterate(T s, Func<T, bool> isDone, Func<T, T> transform)
+        {
+            if (isDone(s))
+                return s;
+
+            s = transform(s);
+            
+            return Iterate(s, isDone, transform);
+        }
+    }
+
+    public static class FactorialControlAbstractionImpl
+    {
+        public static int[] Calculate()
+        {
+            return new ControlAbstraction<int[]>().Iterate(new[] {1,5}, IsDone, Transform);
+        }
+        
+        private static bool IsDone(int[] array) => array[^1] == 1;
+
+        private static int[] Transform(int[] a)
+        {
+            var newArray = new[] {a[0] * a[1], a[1] - 1};
+
+            return newArray;
+        }
+    }
+}
---
+++ DeclarativeParadigm/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DeclarativeParadigm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = FactorialControlAbstractionImpl.Calculate();
+            Console.WriteLine(string.Join(",", result));
+        }
+    }
+}
