05df3b57d1fed451b2cfa5ae3ecbc2bc1c78ed20
Konstantin.Varlamov <k.varlamov88@yandex.ru>
2024-04-27 08:18:54 +0300

    Taxonomy problem

---
+++ OOAP/Poly-Inh/TaxonomyConflict.cs
@@ -0,0 +1,77 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OOAP.Poly_Inh;
+
+internal sealed class TaxonomyConflict
+{
+    public static void StartExample()
+    {
+                
+    }
+}
+
+public class PersonTaxonomyProblem
+{
+    public bool IsResident {get; private set;}
+}
+
+public class PersonFixed
+{
+    public NdsResident Resident {get; set;}
+}
+
+public abstract class NdsResident
+{
+    public abstract decimal GetPercent();
+}
+
+public sealed class RusResident : NdsResident
+{
+    public override decimal GetPercent()
+    {
+        return 13;
+    }
+}
+
+public sealed class SerbResident : NdsResident
+{
+    public override decimal GetPercent()
+    {
+        return 25;
+    }
+}
+
+public class NdsCalculator
+{
+    // Логика, рассчитанная на флаг
+    // - например изначально расчет делался только для одной страны
+    public decimal CalculateWrong(PersonTaxonomyProblem person)
+    {
+        decimal percent;
+        // Неправильно
+        if (person.IsResident)
+        {
+            // calculate percent for resident
+            percent = 13;
+        }
+        else 
+        {
+            // calculate percent for non resident
+            percent = 30;
+        }
+
+        return CalculateNds(percent);
+    }
+    
+    public decimal Calculate(PersonFixed person)
+    {
+        decimal percent = person.Resident.GetPercent();
+        return CalculateNds(percent);
+    }
+    
+    private decimal CalculateNds(decimal percent)
+    {
+        // some logic
+        return decimal.Zero;
+    }
+}
