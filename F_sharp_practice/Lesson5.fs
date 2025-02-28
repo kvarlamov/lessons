d5696a5b75d91c17003cf5da985f3279a50220bd
Konstantin.Varlamov <k.varlamov88@yandex.ru>
2025-02-28 17:34:23 +0300

    Lesson 5

---
+++ F_sharp_practice/Lesson5.fs
@@ -0,0 +1,15 @@
+﻿module F_sharp_practice.Lesson5
+
+// 16.1
+let notDivisible  (n,m) = m % n = 0
+
+// 16.2
+let  prime = function
+    | n when n < 2 -> false
+    | n ->
+        let rec check d =
+            match d with
+            | d when d = n -> true
+            | _ when notDivisible(d, n) -> false
+            | _ -> check (d + 1)
+        check 2
