09b1f19f3bdd623962be6c72332818310d3b038a
Konstantin.Varlamov <k.varlamov88@yandex.ru>
2025-03-04 20:36:57 +0300

    Lesson8

---
+++ F_sharp_practice/Lesson8.fs
@@ -0,0 +1,10 @@
+﻿module F_sharp_practice.Lesson8
+
+//(int * int -> int) -> int -> int ->int
+let curry f =
+    fun (x:int) -> fun (y:int) -> f(x, y) : int
+
+//(int -> int ->int) -> int * int -> int
+let uncurry f =
+    fun (x:int, y:int) -> f x y : int
+
