61fa68889c89c7c70e917613c8d2fe6ac4c86e66
Konstantin.Varlamov <k.varlamov88@yandex.ru>
2025-03-11 21:24:30 +0300

    Lesson 11

---
+++ F_sharp_practice/Lesson11.fs
@@ -0,0 +1,14 @@
+﻿module F_sharp_practice.Lesson11
+
+type F = 
+  | AM
+  | PM
+
+type TimeOfDay = { hours : int; minutes : int; f: F }
+
+let transform (x: TimeOfDay) =
+    match x.f with
+    | AM -> x.hours * 60 + x.minutes
+    | PM -> (x.hours + 12) * 60 + x.minutes
+
+let (.>.) x y = (transform x) > (transform y)
