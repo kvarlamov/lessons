97ac7c130d620ed85b6755c9061b4333d923736d
Konstantin.Varlamov <k.varlamov88@yandex.ru>
2025-03-18 21:25:11 +0300

    [lesson15]

---
+++ F_sharp_practice/Lesson15-folds.fs
@@ -0,0 +1,16 @@
+﻿module F_sharp_practice.Lesson15_HOF
+
+// 41.4.1
+let list_filter f xs =
+    List.foldBack (fun x acc -> if f x then x :: acc else acc) xs []
+
+
+// 41.4.2
+let sum (p, xs) =
+    List.fold (fun acc x -> if p x then x + acc else acc) 0 xs
+
+// 41.4.3
+let revrev = function
+    | mainList ->
+        let innerFold lst = List.fold (fun head tail -> tail :: head) [] lst
+        List.fold (fun acc lst -> (innerFold lst) :: acc) [] mainList
