150536d9d2449daadb06f333ddf3c21895964f8a
Konstantin.Varlamov <k.varlamov88@yandex.ru>
2025-03-20 21:49:05 +0300

    [lesson16]

---
+++ F_sharp_practice/Lesson16-set.fs
@@ -0,0 +1,12 @@
+﻿module F_sharp_practice.Lesson16_set
+
+// 42.3
+let rec allSubsets n k =
+    if k = 0 then [[]]
+    else if  k>n then []
+    else
+        let subsetIncludeN = allSubsets (n-1) (k-1)
+        let subsetWithN = List.map (fun subset -> n :: subset) subsetIncludeN
+        let subsetWithoutN = allSubsets (n-1) k
+        
+        subsetWithN @ subsetWithoutN
