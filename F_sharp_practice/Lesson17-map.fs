3b3ca2cff104d3424ca29b74bc3fd7946f427181
Konstantin.Varlamov <k.varlamov88@yandex.ru>
2025-03-22 08:42:13 +0300

    lesson 17

---
+++ F_sharp_practice/Lesson17-map.fs
@@ -0,0 +1,10 @@
+﻿module F_sharp_practice.Lesson17_map
+
+// 43.3
+let try_find key m =
+    let rec find= function
+        | [] -> None
+        | (k, v) :: tail ->
+            if k = key then Some(v)
+            else find(tail)
+    find (Map.toList m)
