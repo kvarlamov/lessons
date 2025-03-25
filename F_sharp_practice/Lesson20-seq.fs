e3b7fc3a0f9cec4d8436e62f11abaac16921c64d
Konstantin.Varlamov <k.varlamov88@yandex.ru>
2025-03-25 21:23:50 +0300

    lesson20

---
+++ F_sharp_practice/Lesson20-seq.fs
@@ -0,0 +1,19 @@
+﻿module F_sharp_practice.Lesson20
+
+// 49.5.1
+let even_seq = Seq.initInfinite (fun i -> 2 * (i + 1))
+
+// 49.5.2
+let fac_seq =
+    let rec fact = function
+        | 0 | 1 -> 1
+        | x -> x * fact (x - 1)
+
+    Seq.initInfinite fact
+
+// 49.5.3
+let seq_seq = Seq.initInfinite (fun n ->
+    if n = 0 then 0
+    elif n % 2 = 1 then -(n + 1)/2
+    else n /2 )
+    
