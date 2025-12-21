f9fc010e415b6604f1895fda016ce4557a0f2e0d
Konstantin Varlamov <konstantinvarlamov@Konstantins-MacBook-Air.local>
2025-12-21 16:38:50 +0300

    Circuit breaker

---
+++ HardWork/MicroTdd/CircuitBreaker.Tests/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using FluentAssertions;
+
+namespace CircuitBreaker.Tests;
+
+public class UnitTest1
+{
+    private readonly CircuitBreaker _breaker;
+
+    public UnitTest1()
+    {
+        _breaker = new CircuitBreaker(3, TimeSpan.FromSeconds(5));
+    }
+    
+    [Fact]
+    public async Task ResetStateIfTimePassed()
+    {
+        // Arrange
+        _breaker.ManualOpen();
+        _breaker.GetState().Should().BeTrue();
+        
+        await _breaker.ExecuteAsync(() => Task.CompletedTask);
+        _breaker.GetState().Should().BeTrue();
+        
+        //wait to close again
+        await Task.Delay(5000);
+        await _breaker.ExecuteAsync(() => Task.CompletedTask);
+        _breaker.GetState().Should().BeFalse();
+    }
+
+    [Fact]
+    public async Task StateIsOpen_ThrowException()
+    {
+        // Arrange
+        _breaker.ManualOpen();
+        
+        await Assert.ThrowsAsync<InvalidOperationException>(() => _breaker.ExecuteAsync(() => Task.CompletedTask));
+    }
+
+    [Fact]
+    public async Task OnSuccess()
+    {
+        await _breaker.ExecuteAsync(() => Task.CompletedTask);
+        
+        _breaker.GetState().Should().BeFalse();
+        _breaker.GetFailureCount().Should().Be(0);
+    }
+
+    [Fact]
+    public async Task OnFailure()
+    {
+        await Assert.ThrowsAnyAsync<Exception>(() => _breaker.ExecuteAsync(() => throw new InvalidOperationException()));
+        _breaker.GetFailureCount().Should().Be(1);
+    }
+}
---
+++ HardWork/MicroTdd/CircuitBreaker/CircuitBreaker.cs
@@ -0,0 +1,87 @@
+﻿namespace CircuitBreaker;
+
+public sealed class CircuitBreaker
+{
+    // количество попыток до разрыва
+    private readonly int _failureThreshold;
+    // продолжительность разрыва
+    private readonly TimeSpan _openDuration;
+    // счётчик текущих попыток
+    private int _openFailures;
+    private bool _isOpen;
+    private DateTime _openTime;
+
+    public CircuitBreaker(int failureThreshold, TimeSpan? openDuration = null)
+    {
+        _failureThreshold = failureThreshold;
+        _openDuration = openDuration ?? TimeSpan.FromSeconds(5);;
+    }
+
+    public async Task ExecuteAsync(Func<Task> operation)
+    {
+        if (_isOpen)
+        {
+            // если время разрыва закончилось
+            if (DateTime.UtcNow > _openTime + _openDuration)
+            {
+                ResetState();
+            }
+            else
+            {
+                throw new InvalidOperationException("The circuit is open. Please wait");
+            }
+        }
+
+        try
+        {
+            await operation();
+            OnSuccess();
+        }
+        catch (Exception exception)
+        {
+            OnFailure();
+            throw;
+        }
+    }
+
+    private void OnSuccess()
+    {
+        _isOpen = false;
+        _openFailures = 0;
+    }
+
+    private void OnFailure()
+    {
+        _openFailures++;
+        if (_openFailures >= _failureThreshold)
+        {
+            _isOpen = true;
+            _openTime = DateTime.UtcNow;
+        }
+    }
+
+    private void ResetState()
+    {
+        _isOpen = false;
+        _openFailures = 0;
+    }
+
+    /// <summary>
+    /// POST: цепь разорвана (_isOpen=true)
+    /// </summary>
+    public void ManualOpen()
+    {
+        _isOpen = true;
+        _openTime = DateTime.UtcNow;
+    }
+
+    public bool GetState()
+    {
+        return _isOpen;
+    }
+
+    public int GetFailureCount()
+    {
+        return _openFailures;
+    }
+}
