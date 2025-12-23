32dd5db360e0fe70621207680eb96050f376c00d
Konstantin Varlamov <konstantinvarlamov@Konstantins-MacBook-Air.local>
2025-12-23 08:31:58 +0300

    add simple step. Check that doesnot throw

--- HardWork/MicroTdd/SagaPattern.Tests/SagaTests.cs
+++ HardWork/MicroTdd/SagaPattern.Tests/SagaTests.cs
@@ -10,4 +10,14 @@ public class SagaTests
         var saga = new Saga();
         saga.Should().NotBeNull();
     }
+
+    [Fact]
+    public void AddStepToSaga_NotThrowException()
+    {
+        var saga = new Saga();
+        var step = new SagaStep("test", () => { });
+        
+        //Assert that not throw
+        saga.AddStep(step);
+    }
 }
--- HardWork/MicroTdd/SagaPattern/Saga.cs
+++ HardWork/MicroTdd/SagaPattern/Saga.cs
@@ -2,4 +2,24 @@
 
 public sealed class Saga
 {
+    private readonly List<SagaStep>  _steps = new();
+
+    public void AddStep(SagaStep step)
+    {
+        _steps.Add(step);
+    }
+}
+
+public sealed class SagaStep
+{
+    public string Name { get; }
+    public Action Execute { get; }
+    public Action? Compensate { get; }
+
+    public SagaStep(string name, Action execute, Action? compensate = null)
+    {
+        Name = name;
+        Execute = execute;
+        Compensate = compensate;
+    }
 }
